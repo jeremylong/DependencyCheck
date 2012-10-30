96bdd8a41eccc7c9642a11401539893c2bde665b
Jeremy Long <jeremy.long@gmail.com>
2012-10-29 21:49:25 -0400

    removed loopback proxy
    Former-commit-id: c04702bbba9f229eb6efcaa3df0e528089435f6b

--- src/test/java/org/codesecure/dependencycheck/utils/DownloaderIntegrationTest.java
+++ src/test/java/org/codesecure/dependencycheck/utils/DownloaderIntegrationTest.java
@@ -36,7 +36,6 @@ public class DownloaderIntegrationTest {
     public void tearDown() {
     }
 
-   
     /**
      * Test of fetchFile method, of class Downloader.
      * @throws Exception thrown when an excpetion occurs.
@@ -44,14 +43,14 @@ public class DownloaderIntegrationTest {
     @Test
     public void testFetchFile_URL_String() throws Exception {
         System.out.println("fetchFile");
-        
+
 //        Settings.setString(Settings.KEYS.PROXY_URL, "test");
 //        Settings.setString(Settings.KEYS.PROXY_PORT, "80");
 //        Settings.setString(Settings.KEYS.CONNECTION_TIMEOUT, "1000");
-        
-        Settings.setString(Settings.KEYS.PROXY_PORT, "8080");
-        Settings.setString(Settings.KEYS.PROXY_URL, "127.0.0.1");
-        
+
+//        Settings.setString(Settings.KEYS.PROXY_PORT, "8080");
+//        Settings.setString(Settings.KEYS.PROXY_URL, "127.0.0.1");
+
         //URL url = new URL(Settings.getString(Settings.KEYS.CPE_URL));
         URL url = new URL("http://static.nvd.nist.gov/feeds/xml/cve/nvdcve-2.0-modified.xml");
 
