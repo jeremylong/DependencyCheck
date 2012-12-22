5ec9a24c9946ebbc0aa5f417603062f29d416e2a
Jeremy Long <jeremy.long@gmail.com>
2012-12-22 04:30:26 -0500

    continued removal of cpe.xml - it is incomplete for our purpose
    Former-commit-id: 1e1a61f92edfd8ba27530fd53fa0d21bf7862ea4

--- src/test/java/org/codesecure/dependencycheck/dependency/DependencyTest.java
+++ src/test/java/org/codesecure/dependencycheck/dependency/DependencyTest.java
@@ -249,15 +249,13 @@ public class DependencyTest {
         System.out.println("addIdentifier");
         String type = "cpe";
         String value = "cpe:/a:apache:struts:2.1.2";
-        String title = "Apache Struts 2.1.2";
         String url = "http://somewhere";
         Dependency instance = new Dependency();
-        instance.addIdentifier(type, value, title, url);
+        instance.addIdentifier(type, value, url);
         assertEquals(1,instance.getIdentifiers().size());
         Identifier i = instance.getIdentifiers().get(0);
         assertEquals(type,i.getType());
         assertEquals(value, i.getValue());
-        assertEquals(title, i.getTitle());
         assertEquals(url, i.getUrl());
     }
 
