6544cc98d593823c53dd7da28ae432d2f35c62db
Jeremy Long <jeremy.long@gmail.com>
2013-04-18 06:45:41 -0400

    minor update
    Former-commit-id: 69b26d4f5a82c312242fa088306cf3c3628c4796

--- src/main/java/org/owasp/dependencycheck/reporting/ReportGenerator.java
+++ src/main/java/org/owasp/dependencycheck/reporting/ReportGenerator.java
@@ -27,6 +27,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
