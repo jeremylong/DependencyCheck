0c100c1372473da105b64ec9be84c82953c52432
Jeremy Long <jeremy.long@gmail.com>
2013-05-10 05:52:44 -0400

    updated file header comment
    Former-commit-id: 7398d863e1b4271bd39875644f2de3d3376d7e26

--- src/test/java/org/owasp/dependencycheck/dependency/DependencyTest.java
+++ src/test/java/org/owasp/dependencycheck/dependency/DependencyTest.java
@@ -1,3 +1,21 @@
+/*
+ * This file is part of Dependency-Check.
+ *
+ * Dependency-Check is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * Dependency-Check is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * DependencyCheck. If not, see http://www.gnu.org/licenses/.
+ *
+ * Copyright (c) 2013 Jeremy Long. All Rights Reserved.
+ */
 package org.owasp.dependencycheck.dependency;
 
 import java.util.Set;
