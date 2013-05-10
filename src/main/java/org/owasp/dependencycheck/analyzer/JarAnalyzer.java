d180618634c4ca3864098989052636937a57b0a2
Jeremy Long <jeremy.long@gmail.com>
2013-05-10 05:52:44 -0400

    updated file header comment
    Former-commit-id: ef8657a90b398f59cc0693f0a094292609fdd9ef

--- src/main/java/org/owasp/dependencycheck/analyzer/JarAnalyzer.java
+++ src/main/java/org/owasp/dependencycheck/analyzer/JarAnalyzer.java
@@ -1,12 +1,12 @@
 /*
- * This file is part of DependencyCheck.
+ * This file is part of Dependency-Check.
  *
- * DependencyCheck is free software: you can redistribute it and/or modify it
+ * Dependency-Check is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
  * Software Foundation, either version 3 of the License, or (at your option) any
  * later version.
  *
- * DependencyCheck is distributed in the hope that it will be useful, but
+ * Dependency-Check is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
