37d165d6cbba9bdc9a87a5a9a8d52067f232fcbd
Jeremy Long <jeremy.long@gmail.com>
2013-06-04 23:40:02 -0400

    updated javadoc
    Former-commit-id: 89eab3c72a4de3c95e5debc38e937166a93ad47d

---
+++ src/main/java/org/owasp/dependencycheck/analyzer/pom/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * <html>
+ * <head>
+ * <title>org.owasp.dependencycheck.analyzer.pom</title>
+ * </head>
+ * <body>
+ * This package contains utility classes used to parse pom.xml files.
+ * </body>
+ * </html>
+*/
+
+package org.owasp.dependencycheck.analyzer.pom;
