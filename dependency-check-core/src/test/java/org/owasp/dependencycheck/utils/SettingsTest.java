0436a095b54ef13ec28b980e14f73c8c46f43af3
Jeremy Long <jeremy.long@gmail.com>
2013-08-29 17:01:47 -0400

    added test case for removeProperty
    Former-commit-id: e48ae0be2da9aaab3d51c7b57dcb2919b72fbeb2

--- dependency-check-core/src/test/java/org/owasp/dependencycheck/utils/SettingsTest.java
+++ dependency-check-core/src/test/java/org/owasp/dependencycheck/utils/SettingsTest.java
@@ -163,4 +163,20 @@ public class SettingsTest {
         boolean result = Settings.getBoolean(key);
         Assert.assertEquals(expResult, result);
     }
+
+    /**
+     * Test of removeProperty method, of class Settings.
+     */
+    @Test
+    public void testRemoveProperty() {
+        String key = "SomeKey";
+        String value = "value";
+        String dfault = "default";
+        Settings.setString(key, value);
+        String ret = Settings.getString(key);
+        Assert.assertEquals(value, ret);
+        Settings.removeProperty(key);
+        ret = Settings.getString(key, dfault);
+        Assert.assertEquals(dfault, ret);
+    }
 }
