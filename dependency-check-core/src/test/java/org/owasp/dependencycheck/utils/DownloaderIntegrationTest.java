6f94faee14f93b9360ad502e4daf772ca6bb8b36
Jeremy Long <jeremy.long@gmail.com>
2013-09-02 16:19:47 -0400

    fixed git merge problem
    Former-commit-id: c9b325d28586a399bd666dbe235e73913f26b81e

--- dependency-check-core/src/test/java/org/owasp/dependencycheck/utils/DownloaderIntegrationTest.java
+++ dependency-check-core/src/test/java/org/owasp/dependencycheck/utils/DownloaderIntegrationTest.java
@@ -18,6 +18,7 @@
  */
 package org.owasp.dependencycheck.utils;
 
+import java.io.File;
 import org.owasp.dependencycheck.utils.Settings;
 import org.owasp.dependencycheck.utils.Downloader;
 import java.net.URL;
@@ -59,17 +60,12 @@ public class DownloaderIntegrationTest {
     public void testFetchFile() throws Exception {
 
 //        Settings.setString(Settings.KEYS.CONNECTION_TIMEOUT, "1000");
-
 //        Settings.setString(Settings.KEYS.PROXY_PORT, "8080");
 //        Settings.setString(Settings.KEYS.PROXY_URL, "127.0.0.1");
 
-        URL url = new URL(Settings.getString(Settings.KEYS.CPE_URL));
-        String outputPath = "target/downloaded_cpe.xml";
-        Downloader.fetchFile(url, outputPath, true);
-
-        url = new URL(Settings.getString(Settings.KEYS.CVE_MODIFIED_20_URL));
-        outputPath = "target/downloaded_cve.xml";
-        Downloader.fetchFile(url, outputPath, false);
+        URL url = new URL(Settings.getString(Settings.KEYS.CVE_MODIFIED_20_URL));
+        File outputPath = new File("target/downloaded_cve.xml");
+        Downloader.fetchFile(url, outputPath);
 
     }
 
