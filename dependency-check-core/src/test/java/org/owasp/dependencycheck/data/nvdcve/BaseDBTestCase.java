e871d370440b75aebbee4b383b1c21c893d407cd
Jeremy Long <jeremy.long@gmail.com>
2013-11-28 06:22:50 -0500

    renamged getFile to getDataFle (settings class)
    Former-commit-id: 8b632f78685d0a66bc061bcefd46de4b56d45347

--- dependency-check-core/src/test/java/org/owasp/dependencycheck/data/nvdcve/BaseDBTestCase.java
+++ dependency-check-core/src/test/java/org/owasp/dependencycheck/data/nvdcve/BaseDBTestCase.java
@@ -49,7 +49,7 @@ public abstract class BaseDBTestCase extends TestCase {
 
     public static void ensureDBExists() throws Exception {
 
-        java.io.File dataPath = Settings.getFile(Settings.KEYS.DATA_DIRECTORY);
+        java.io.File dataPath = Settings.getDataFile(Settings.KEYS.DATA_DIRECTORY);
         if (!dataPath.exists() || (dataPath.isDirectory() && dataPath.listFiles().length < 3)) {
             dataPath.mkdirs();
             FileInputStream fis = null;
