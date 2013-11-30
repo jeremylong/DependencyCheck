cff4f29ba47e64cad7c4a9f43f56ea8e76d08be0
Jeremy Long <jeremy.long@gmail.com>
2013-11-30 08:53:46 -0500

    added new package
    Former-commit-id: 966169962b5ef6f51366048aa7734f41ac162e8a

---
+++ dependency-check-core/src/main/java/org/owasp/dependencycheck/suppression/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * <html>
+ * <head>
+ * <title>org.owasp.dependencycheck.suppression</title>
+ * </head>
+ * <body>
+ * Contains classes used to suppress findings.
+ * </body>
+ * </html>
+ */
+package org.owasp.dependencycheck.suppression;
