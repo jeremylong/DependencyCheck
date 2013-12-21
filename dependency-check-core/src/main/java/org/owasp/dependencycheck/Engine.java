f1e0b7a94fd9b19e1093e896ed0a833438c2ae22
Jeremy Long <jeremy.long@gmail.com>
2013-12-21 07:46:47 -0500

    made error handling more robust so analysis should stop when something goes wrong with a signle jar
    Former-commit-id: f873948c981239f37fb0083b34906be31c0e9ca1

--- dependency-check-core/src/main/java/org/owasp/dependencycheck/Engine.java
+++ dependency-check-core/src/main/java/org/owasp/dependencycheck/Engine.java
@@ -336,6 +336,12 @@ public class Engine {
                             a.analyze(d, this);
                         } catch (AnalysisException ex) {
                             d.addAnalysisException(ex);
+                        } catch (Throwable ex) {
+                            final String axMsg = String.format("An unexpected error occured during analysis of '%s'", d.getActualFilePath());
+                            AnalysisException ax = new AnalysisException(axMsg, ex);
+                            d.addAnalysisException(ax);
+                            Logger.getLogger(Engine.class.getName()).log(Level.SEVERE, axMsg);
+                            Logger.getLogger(Engine.class.getName()).log(Level.FINE, axMsg, ex);
                         }
                     }
                 }
