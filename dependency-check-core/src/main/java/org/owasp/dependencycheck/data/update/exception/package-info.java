ded3079390fbcdf431ca3a908c80369127557eb6
Jeremy Long <jeremy.long@gmail.com>
2014-01-04 09:17:02 -0500

    updated package-info
    Former-commit-id: cbacf975eae9951165dffdda5ffe0066589d8268

---
+++ dependency-check-core/src/main/java/org/owasp/dependencycheck/data/update/exception/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * <html>
+ * <head>
+ * <title>org.owasp.dependencycheck.data.update.exception</title>
+ * </head>
+ * <body>
+ * <p>A collection of exception classes used within the application.</p>
+ * </body>
+ * </html>
+ */
+package org.owasp.dependencycheck.data.update.exception;
---
+++ dependency-check-core/src/main/java/org/owasp/dependencycheck/exception/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * <html>
+ * <head>
+ * <title>org.owasp.dependencycheck.exception</title>
+ * </head>
+ * <body>
+ * <p>A collection of exception classes used within the application.</p>
+ * </body>
+ * </html>
+ */
+package org.owasp.dependencycheck.exception;
