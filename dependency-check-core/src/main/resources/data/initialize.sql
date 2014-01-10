98b4509014a0b76b452b5ab0c2919731f0bfaf8b
Jeremy Long <jeremy.long@gmail.com>
2014-01-10 06:16:32 -0500

    updated to make SQL statements more cross platform
    Former-commit-id: 5cf3f06366bfd0b2d474891266efea9459aec26a

--- dependency-check-core/src/main/resources/data/initialize.sql
+++ dependency-check-core/src/main/resources/data/initialize.sql
@@ -1,16 +1,8 @@
-DROP INDEX IF EXISTS idxVulnerability;
-DROP INDEX IF EXISTS idxReference;
-DROP INDEX IF EXISTS idxCpe;
-DROP INDEX IF EXISTS idxCpeEntry;
-DROP INDEX IF EXISTS idxSoftwareCve;
-DROP INDEX IF EXISTS idxSoftwareCpe;
-DROP TABLE IF EXISTS vulnerability;
-DROP TABLE IF EXISTS reference;
-DROP TABLE IF EXISTS cpeEntry;
 DROP TABLE IF EXISTS software;
+DROP TABLE IF EXISTS cpeEntry;
+DROP TABLE IF EXISTS reference;
+DROP TABLE IF EXISTS vulnerability;
 DROP TABLE IF EXISTS properties;
-
-CREATE TABLE properties (id varchar(50) PRIMARY KEY, value varchar(500));
 
 CREATE TABLE vulnerability (id int auto_increment PRIMARY KEY, cve VARCHAR(20) UNIQUE,
 	description VARCHAR(8000), cwe VARCHAR(10), cvssScore DECIMAL(3,1), cvssAccessVector VARCHAR(20),
@@ -20,7 +12,7 @@ CREATE TABLE vulnerability (id int auto_increment PRIMARY KEY, cve VARCHAR(20) U
 CREATE TABLE reference (cveid INT, name VARCHAR(1000), url VARCHAR(1000), source VARCHAR(255),
 	CONSTRAINT fkReference FOREIGN KEY (cveid) REFERENCES vulnerability(id) ON DELETE CASCADE);
 
-CREATE TABLE cpeEntry (id INT auto_increment PRIMARY KEY, cpe VARCHAR(500), vendor VARCHAR(255), product VARCHAR(255));
+CREATE TABLE cpeEntry (id INT auto_increment PRIMARY KEY, cpe VARCHAR(250), vendor VARCHAR(255), product VARCHAR(255));
 
 CREATE TABLE software (cveid INT, cpeEntryId INT, previousVersion VARCHAR(50)
     , CONSTRAINT fkSoftwareCve FOREIGN KEY (cveid) REFERENCES vulnerability(id) ON DELETE CASCADE
@@ -32,3 +24,6 @@ CREATE INDEX idxCpe ON cpeEntry(cpe);
 CREATE INDEX idxCpeEntry ON cpeEntry(vendor, product);
 CREATE INDEX idxSoftwareCve ON software(cveid);
 CREATE INDEX idxSoftwareCpe ON software(cpeEntryId);
+
+CREATE TABLE properties (id varchar(50) PRIMARY KEY, value varchar(500));
+INSERT INTO properties(id, value) VALUES ('version', '2.8');
