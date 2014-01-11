11a634b9be6c495f5bcb56831d5a9790395bc352
Jeremy Long <jeremy.long@gmail.com>
2014-01-11 12:41:30 -0500

    updated package info
    Former-commit-id: 61a2ac8c1d1888e508118805752d67e23e63bce3

--- dependency-check-core/src/main/java/org/owasp/dependencycheck/data/update/xml/package-info.java
+++ dependency-check-core/src/main/java/org/owasp/dependencycheck/data/update/xml/package-info.java
@@ -5,14 +5,12 @@
  * </head>
  * <body>
  * <p>Contains classes used to parse the NVD CVE XML file.</p>
- * <p>The basic use is that the Importer is called to import
- * an NVD CVE file. The Importer instantiates an Indexer object
- * (which extends Index). The Indexer creates a partial-unmarshalling
- * SAX parser (implemented in the NvdCveXmlFilter) that extracts
- * VulnerabilityTypes (aka Entry) from the NVD CVE data file and
- * stores these into a Lucene Index.</p>
+ * <p>The basic use is that the Importer is called to import an NVD CVE file.
+ * The Importer instantiates an Indexer object (which extends Index). The
+ * Indexer creates a partial-unmarshalling SAX parser (implemented in the
+ * NvdCveXmlFilter) that extracts VulnerabilityTypes (aka Entry) from the NVD
+ * CVE data file and stores these into a Lucene Index.</p>
  * </body>
  * </html>
-*/
-
-package org.owasp.dependencycheck.data.update;
+ */
+package org.owasp.dependencycheck.data.update.xml;
