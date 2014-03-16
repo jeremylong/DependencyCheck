cb56bbc122d2187f0c63f93345bb8a447ee250af
Jeremy Long <jeremy.long@gmail.com>
2014-03-16 07:04:36 -0400

    updated class name
    Former-commit-id: 7cda28ba656b243a6b64d1101cd81a2868837c2c

---
+++ dependency-check-core/src/test/java/org/owasp/dependencycheck/data/cpe/AbstractDatabaseTestCase.java
@@ -0,0 +1,55 @@
+/*
+ * This file is part of dependency-check-core.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright (c) 2012 Jeremy Long. All Rights Reserved.
+ */
+package org.owasp.dependencycheck.data.cpe;
+
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.owasp.dependencycheck.data.nvdcve.BaseDBTestCase;
+
+/**
+ * An abstract database test case that is used to ensure the H2 DB exists prior to performing tests that utilize the
+ * data contained within.
+ *
+ * @author Jeremy Long <jeremy.long@owasp.org>
+ */
+public abstract class AbstractDatabaseTestCase extends TestCase {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    @Override
+    public void setUp() throws Exception {
+        BaseDBTestCase.ensureDBExists();
+        super.setUp();
+    }
+
+    @After
+    @Override
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+}
