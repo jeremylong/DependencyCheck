56a43fe17b1c073d23f072018a0b8c16140dd2ad
Jeremy Long <jeremy.long@gmail.com>
2014-03-29 05:34:46 -0400

    checkstyle corrections
    Former-commit-id: c49596f06b497f80dbe6b5b9656a5221312c7e3f

---
+++ dependency-check-core/src/main/java/org/owasp/dependencycheck/agent/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * <html>
+ * <head>
+ * <title>org.owasp.dependencycheck.agent</title>
+ * </head>
+ * <body>
+ * The agent package holds an agent API that can be used by other applications that have information about dependencies;
+ * but would rather implement something in their code directly rather then spawn a process to run the entire
+ * dependency-check engine. This basically provides programmatic access to running a scan.
+ * </body>
+ * </html>
+ */
+package org.owasp.dependencycheck.agent;
