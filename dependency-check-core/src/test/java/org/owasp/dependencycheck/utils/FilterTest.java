691636de7bb5fec2d7a82708ea01cc4ca6091175
Jeremy Long <jeremy.long@gmail.com>
2014-04-16 07:56:51 -0400

    removed unused methods
    Former-commit-id: 6e0577ad17ed28f5e6e4f72fa35c10c5250343b4

--- dependency-check-core/src/test/java/org/owasp/dependencycheck/utils/FilterTest.java
+++ dependency-check-core/src/test/java/org/owasp/dependencycheck/utils/FilterTest.java
@@ -19,13 +19,9 @@ package org.owasp.dependencycheck.utils;
 
 import java.util.ArrayList;
 import java.util.List;
-import org.junit.After;
-import org.junit.AfterClass;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
@@ -33,25 +29,6 @@ import org.junit.Test;
  * @author Jeremy Long <jeremy.long@owasp.org>
  */
 public class FilterTest {
-
-    public FilterTest() {
-    }
-
-    @BeforeClass
-    public static void setUpClass() throws Exception {
-    }
-
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-    }
-
-    @Before
-    public void setUp() {
-    }
-
-    @After
-    public void tearDown() {
-    }
 
     /**
      * Test of passes method, of class Filter.
