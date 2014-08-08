3ce85d8ca943b7631888bfe2cf62db44272f866b
Jeremy Long <jeremy.long@gmail.com>
2014-08-08 14:42:23 -0400

    rework of report generation and added fix for proxy (patch for proxy was from Erik Wramner) to close issue #136
    Former-commit-id: afc81123b31189618ade397b830bf421db2918f8

--- dependency-check-maven/src/main/java/org/owasp/dependencycheck/maven/DependencyCheckMojo.java
+++ dependency-check-maven/src/main/java/org/owasp/dependencycheck/maven/DependencyCheckMojo.java
@@ -20,10 +20,6 @@ package org.owasp.dependencycheck.maven;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Set;
@@ -46,16 +42,10 @@ import org.apache.maven.reporting.MavenReport;
 import org.apache.maven.reporting.MavenReportException;
 import org.apache.maven.settings.Proxy;
 import org.owasp.dependencycheck.Engine;
-import org.owasp.dependencycheck.data.nvdcve.CveDB;
 import org.owasp.dependencycheck.data.nvdcve.DatabaseException;
-import org.owasp.dependencycheck.data.nvdcve.DatabaseProperties;
 import org.owasp.dependencycheck.dependency.Dependency;
-import org.owasp.dependencycheck.dependency.Evidence;
 import org.owasp.dependencycheck.dependency.Identifier;
-import org.owasp.dependencycheck.dependency.Reference;
 import org.owasp.dependencycheck.dependency.Vulnerability;
-import org.owasp.dependencycheck.dependency.VulnerableSoftware;
-import org.owasp.dependencycheck.reporting.ReportGenerator;
 import org.owasp.dependencycheck.utils.LogUtils;
 import org.owasp.dependencycheck.utils.Settings;
 
@@ -72,7 +62,7 @@ public class DependencyCheckMojo extends AbstractMojo implements MavenMultiPageR
     /**
      * Logger field reference.
      */
-    private final Logger logger = Logger.getLogger(DependencyCheckMojo.class.getName());
+    private static final Logger logger = Logger.getLogger(DependencyCheckMojo.class.getName());
 
     /**
      * The properties file location.
@@ -359,408 +349,6 @@ public class DependencyCheckMojo extends AbstractMojo implements MavenMultiPageR
     }
 
     /**
-     * Generates the reports for a given dependency-check engine.
-     *
-     * @param engine a dependency-check engine
-     * @param outDirectory the directory to write the reports to
-     */
-    private void generateExternalReports(Engine engine, File outDirectory) {
-        DatabaseProperties prop = null;
-        CveDB cve = null;
-        try {
-            cve = new CveDB();
-            cve.open();
-            prop = cve.getDatabaseProperties();
-        } catch (DatabaseException ex) {
-            logger.log(Level.FINE, "Unable to retrieve DB Properties", ex);
-        } finally {
-            if (cve != null) {
-                cve.close();
-            }
-        }
-        final ReportGenerator r = new ReportGenerator(project.getName(), engine.getDependencies(), engine.getAnalyzers(), prop);
-        try {
-            r.generateReports(outDirectory.getCanonicalPath(), format);
-        } catch (IOException ex) {
-            logger.log(Level.SEVERE,
-                    "Unexpected exception occurred during analysis; please see the verbose error log for more details.");
-            logger.log(Level.FINE, null, ex);
-        } catch (Throwable ex) {
-            logger.log(Level.SEVERE,
-                    "Unexpected exception occurred during analysis; please see the verbose error log for more details.");
-            logger.log(Level.FINE, null, ex);
-        }
-    }
-
-    /**
-     * Generates a dependency-check report using the Maven Site format.
-     *
-     * @param engine the engine used to scan the dependencies
-     * @param sink the sink to write the data to
-     */
-    private void generateMavenSiteReport(final Engine engine, Sink sink) {
-        final List<Dependency> dependencies = engine.getDependencies();
-
-        writeSiteReportHeader(sink, project.getName());
-        writeSiteReportTOC(sink, dependencies);
-
-        int cnt = 0;
-        for (Dependency d : dependencies) {
-            writeSiteReportDependencyHeader(sink, d);
-            cnt = writeSiteReportDependencyEvidenceUsed(d, cnt, sink);
-            cnt = writeSiteReportDependencyRelatedDependencies(d, cnt, sink);
-            writeSiteReportDependencyIdentifiers(d, sink);
-            writeSiteReportDependencyVulnerabilities(d, sink, cnt);
-        }
-        sink.body_();
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="various writeXXXXX methods to generate the Site Report">
-    /**
-     * Writes the vulnerabilities to the site report.
-     *
-     * @param d the dependency
-     * @param sink the sink to write the data to
-     * @param collapsibleHeaderCount the collapsible header count
-     */
-    private void writeSiteReportDependencyVulnerabilities(Dependency d, Sink sink, int collapsibleHeaderCount) {
-        int cnt = collapsibleHeaderCount;
-        if (d.getVulnerabilities() != null && !d.getVulnerabilities().isEmpty()) {
-            for (Vulnerability v : d.getVulnerabilities()) {
-
-                sink.paragraph();
-                sink.bold();
-                try {
-                    sink.link("http://web.nvd.nist.gov/view/vuln/detail?vulnId=" + URLEncoder.encode(v.getName(), "US-ASCII"));
-                    sink.text(v.getName());
-                    sink.link_();
-                    sink.bold_();
-                } catch (UnsupportedEncodingException ex) {
-                    sink.text(v.getName());
-                    sink.bold_();
-                    sink.lineBreak();
-                    sink.text("http://web.nvd.nist.gov/view/vuln/detail?vulnId=" + v.getName());
-                }
-                sink.paragraph_();
-                sink.paragraph();
-                sink.text("Severity: ");
-                if (v.getCvssScore() < 4.0) {
-                    sink.text("Low");
-                } else {
-                    if (v.getCvssScore() >= 7.0) {
-                        sink.text("High");
-                    } else {
-                        sink.text("Medium");
-                    }
-                }
-                sink.lineBreak();
-                sink.text("CVSS Score: " + v.getCvssScore());
-                if (v.getCwe() != null && !v.getCwe().isEmpty()) {
-                    sink.lineBreak();
-                    sink.text("CWE: ");
-                    sink.text(v.getCwe());
-                }
-                sink.paragraph_();
-                sink.paragraph();
-                sink.text(v.getDescription());
-                if (v.getReferences() != null && !v.getReferences().isEmpty()) {
-                    sink.list();
-                    for (Reference ref : v.getReferences()) {
-                        sink.listItem();
-                        sink.text(ref.getSource());
-                        sink.text(" - ");
-                        sink.link(ref.getUrl());
-                        sink.text(ref.getName());
-                        sink.link_();
-                        sink.listItem_();
-                    }
-                    sink.list_();
-                }
-                sink.paragraph_();
-                if (v.getVulnerableSoftware() != null && !v.getVulnerableSoftware().isEmpty()) {
-                    sink.paragraph();
-
-                    cnt += 1;
-                    sink.rawText("Vulnerable Software <a href=\"javascript:toggleElement(this, 'vulnSoft" + cnt + "')\">[-]</a>");
-                    sink.rawText("<div id=\"vulnSoft" + cnt + "\" style=\"display:block\">");
-                    sink.list();
-                    for (VulnerableSoftware vs : v.getVulnerableSoftware()) {
-                        sink.listItem();
-                        try {
-                            sink.link("http://web.nvd.nist.gov/view/vuln/search-results?cpe=" + URLEncoder.encode(vs.getName(), "US-ASCII"));
-                            sink.text(vs.getName());
-                            sink.link_();
-                            if (vs.hasPreviousVersion()) {
-                                sink.text(" and all previous versions.");
-                            }
-                        } catch (UnsupportedEncodingException ex) {
-                            sink.text(vs.getName());
-                            if (vs.hasPreviousVersion()) {
-                                sink.text(" and all previous versions.");
-                            }
-                            sink.text(" (http://web.nvd.nist.gov/view/vuln/search-results?cpe=" + vs.getName() + ")");
-                        }
-
-                        sink.listItem_();
-                    }
-                    sink.list_();
-                    sink.rawText("</div>");
-                    sink.paragraph_();
-                }
-            }
-        }
-    }
-
-    /**
-     * Writes the identifiers to the site report.
-     *
-     * @param d the dependency
-     * @param sink the sink to write the data to
-     */
-    private void writeSiteReportDependencyIdentifiers(Dependency d, Sink sink) {
-        if (d.getIdentifiers() != null && !d.getIdentifiers().isEmpty()) {
-            sink.sectionTitle4();
-            sink.text("Identifiers");
-            sink.sectionTitle4_();
-            sink.list();
-            for (Identifier i : d.getIdentifiers()) {
-                sink.listItem();
-                sink.text(i.getType());
-                sink.text(": ");
-                if (i.getUrl() != null && i.getUrl().length() > 0) {
-                    sink.link(i.getUrl());
-                    sink.text(i.getValue());
-                    sink.link_();
-                } else {
-                    sink.text(i.getValue());
-                }
-                if (i.getDescription() != null && i.getDescription().length() > 0) {
-                    sink.lineBreak();
-                    sink.text(i.getDescription());
-                }
-                sink.listItem_();
-            }
-            sink.list_();
-        }
-    }
-
-    /**
-     * Writes the related dependencies to the site report.
-     *
-     * @param d the dependency
-     * @param sink the sink to write the data to
-     * @param collapsibleHeaderCount the collapsible header count
-     * @return the collapsible header count
-     */
-    private int writeSiteReportDependencyRelatedDependencies(Dependency d, int collapsibleHeaderCount, Sink sink) {
-        int cnt = collapsibleHeaderCount;
-        if (d.getRelatedDependencies() != null && !d.getRelatedDependencies().isEmpty()) {
-            cnt += 1;
-            sink.sectionTitle4();
-            sink.rawText("Related Dependencies <a href=\"javascript:toggleElement(this, 'related" + cnt + "')\">[+]</a>");
-            sink.sectionTitle4_();
-            sink.rawText("<div id=\"related" + cnt + "\" style=\"display:none\">");
-            sink.list();
-            for (Dependency r : d.getRelatedDependencies()) {
-                sink.listItem();
-                sink.text(r.getFileName());
-                sink.list();
-                writeListItem(sink, "File Path: " + r.getFilePath());
-                writeListItem(sink, "SHA1: " + r.getSha1sum());
-                writeListItem(sink, "MD5: " + r.getMd5sum());
-                sink.list_();
-                sink.listItem_();
-            }
-            sink.list_();
-            sink.rawText("</div>");
-        }
-        return cnt;
-    }
-
-    /**
-     * Writes the evidence used to the site report.
-     *
-     * @param d the dependency
-     * @param sink the sink to write the data to
-     * @param collapsibleHeaderCount the collapsible header count
-     * @return the collapsible header count
-     */
-    private int writeSiteReportDependencyEvidenceUsed(Dependency d, int collapsibleHeaderCount, Sink sink) {
-        int cnt = collapsibleHeaderCount;
-        final Set<Evidence> evidence = d.getEvidenceForDisplay();
-        if (evidence != null && evidence.size() > 0) {
-            cnt += 1;
-            sink.sectionTitle4();
-            sink.rawText("Evidence Collected <a href=\"javascript:toggleElement(this, 'evidence" + cnt + "')\">[+]</a>");
-            sink.sectionTitle4_();
-            sink.rawText("<div id=\"evidence" + cnt + "\" style=\"display:none\">");
-            sink.table();
-            sink.tableRow();
-            writeTableHeaderCell(sink, "Source");
-            writeTableHeaderCell(sink, "Name");
-            writeTableHeaderCell(sink, "Value");
-            sink.tableRow_();
-            for (Evidence e : evidence) {
-                sink.tableRow();
-                writeTableCell(sink, e.getSource());
-                writeTableCell(sink, e.getName());
-                writeTableCell(sink, e.getValue());
-                sink.tableRow_();
-            }
-            sink.table_();
-            sink.rawText("</div>");
-        }
-        return cnt;
-    }
-
-    /**
-     * Writes the dependency header to the site report.
-     *
-     * @param d the dependency
-     * @param sink the sink to write the data to
-     */
-    private void writeSiteReportDependencyHeader(Sink sink, Dependency d) {
-        sink.sectionTitle2();
-        sink.anchor("sha1" + d.getSha1sum());
-        sink.text(d.getFileName());
-        sink.anchor_();
-        sink.sectionTitle2_();
-        if (d.getDescription() != null && d.getDescription().length() > 0) {
-            sink.paragraph();
-            sink.bold();
-            sink.text("Description: ");
-            sink.bold_();
-            sink.text(d.getDescription());
-            sink.paragraph_();
-        }
-        if (d.getLicense() != null && d.getLicense().length() > 0) {
-            sink.paragraph();
-            sink.bold();
-            sink.text("License: ");
-            sink.bold_();
-            if (d.getLicense().startsWith("http://") && !d.getLicense().contains(" ")) {
-                sink.link(d.getLicense());
-                sink.text(d.getLicense());
-                sink.link_();
-            } else {
-                sink.text(d.getLicense());
-            }
-            sink.paragraph_();
-        }
-    }
-
-    /**
-     * Adds a list item to the site report.
-     *
-     * @param sink the sink to write the data to
-     * @param text the text to write
-     */
-    private void writeListItem(Sink sink, String text) {
-        sink.listItem();
-        sink.text(text);
-        sink.listItem_();
-    }
-
-    /**
-     * Adds a table cell to the site report.
-     *
-     * @param sink the sink to write the data to
-     * @param text the text to write
-     */
-    private void writeTableCell(Sink sink, String text) {
-        sink.tableCell();
-        sink.text(text);
-        sink.tableCell_();
-    }
-
-    /**
-     * Adds a table header cell to the site report.
-     *
-     * @param sink the sink to write the data to
-     * @param text the text to write
-     */
-    private void writeTableHeaderCell(Sink sink, String text) {
-        sink.tableHeaderCell();
-        sink.text(text);
-        sink.tableHeaderCell_();
-    }
-
-    /**
-     * Writes the TOC for the site report.
-     *
-     * @param sink the sink to write the data to
-     * @param dependencies the dependencies that are being reported on
-     */
-    private void writeSiteReportTOC(Sink sink, final List<Dependency> dependencies) {
-        sink.list();
-        for (Dependency d : dependencies) {
-            sink.listItem();
-            sink.link("#sha1" + d.getSha1sum());
-            sink.text(d.getFileName());
-            sink.link_();
-            if (!d.getVulnerabilities().isEmpty()) {
-                sink.rawText(" <font style=\"color:red\">•</font>");
-            }
-            if (!d.getRelatedDependencies().isEmpty()) {
-                sink.list();
-                for (Dependency r : d.getRelatedDependencies()) {
-                    writeListItem(sink, r.getFileName());
-                }
-                sink.list_();
-            }
-            sink.listItem_();
-        }
-        sink.list_();
-    }
-
-    /**
-     * Writes the site report header.
-     *
-     * @param sink the sink to write the data to
-     * @param projectName the name of the project
-     */
-    private void writeSiteReportHeader(Sink sink, String projectName) {
-        sink.head();
-        sink.title();
-        sink.text("Dependency-Check Report: " + projectName);
-        sink.title_();
-        sink.head_();
-        sink.body();
-        sink.rawText("<script type=\"text/javascript\">");
-        sink.rawText("function toggleElement(el, targetId) {");
-        sink.rawText("if (el.innerText == '[+]') {");
-        sink.rawText("    el.innerText = '[-]';");
-        sink.rawText("    document.getElementById(targetId).style.display='block';");
-        sink.rawText("} else {");
-        sink.rawText("    el.innerText = '[+]';");
-        sink.rawText("    document.getElementById(targetId).style.display='none';");
-        sink.rawText("}");
-
-        sink.rawText("}");
-        sink.rawText("</script>");
-        sink.section1();
-        sink.sectionTitle1();
-        sink.text("Project: " + projectName);
-        sink.sectionTitle1_();
-        sink.date();
-        final Date now = new Date();
-        sink.text(DateFormat.getDateTimeInstance().format(now));
-        sink.date_();
-        sink.section1_();
-    }
-    // </editor-fold>
-
-    /**
-     * Returns the maven settings proxy server.
-     *
-     * @param proxy the maven proxy
-     * @return the proxy url
-     */
-    private String getMavenSettingsProxyServer(Proxy proxy) {
-        return new StringBuilder(proxy.getProtocol()).append("://").append(proxy.getHost()).toString();
-    }
-
-    /**
      * Returns the maven proxy.
      *
      * @return the maven proxy
@@ -816,14 +404,17 @@ public class DependencyCheckMojo extends AbstractMojo implements MavenMultiPageR
 
         final Proxy proxy = getMavenProxy();
         if (proxy != null) {
-            Settings.setString(Settings.KEYS.PROXY_SERVER, getMavenSettingsProxyServer(proxy));
+            Settings.setString(Settings.KEYS.PROXY_SERVER, proxy.getHost());
             Settings.setString(Settings.KEYS.PROXY_PORT, Integer.toString(proxy.getPort()));
             final String userName = proxy.getUsername();
             final String password = proxy.getPassword();
-            if (userName != null && password != null) {
+            if (userName != null) {
                 Settings.setString(Settings.KEYS.PROXY_USERNAME, userName);
+            }
+            if (password != null) {
                 Settings.setString(Settings.KEYS.PROXY_PASSWORD, password);
             }
+
         }
 
         if (connectionTimeout != null && !connectionTimeout.isEmpty()) {
