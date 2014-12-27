5d920e4b445e4a5921b82714a69d0436c1e0d5a6
Jeremy Long <jeremy.long@gmail.com>
2014-12-27 06:44:03 -0500

    added BaseTest to initialize the Settings for tests in the maven-plugin
    Former-commit-id: 7da3f4712abcbebf157c8d26174cf0fe2f19f12b

---
+++ dependency-check-maven/src/test/java/org/owasp/dependencycheck/maven/BaseTest.java
@@ -0,0 +1,47 @@
+/*
+ * This file is part of dependency-check-maven.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright (c) 2014 Jeremy Long. All Rights Reserved.
+ */
+package org.owasp.dependencycheck.maven;
+
+import java.io.InputStream;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.owasp.dependencycheck.utils.Settings;
+
+/**
+ *
+ * @author Jeremy Long <jeremy.long@owasp.org>
+ */
+class BaseTest {
+
+    /**
+     * The properties file location.
+     */
+    public static final String PROPERTIES_FILE = "mojo.properties";
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        Settings.initialize();
+        InputStream mojoProperties = BaseTest.class.getClassLoader().getResourceAsStream(BaseTest.PROPERTIES_FILE);
+        Settings.mergeProperties(mojoProperties);
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+        Settings.cleanup(true);
+    }
+}
