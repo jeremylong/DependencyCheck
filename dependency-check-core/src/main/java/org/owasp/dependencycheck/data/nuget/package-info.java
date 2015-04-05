725949db2fd9435cd93a31ac4827e35cd181df56
Jeremy Long <jeremy.long@gmail.com>
2015-04-05 09:31:30 -0400

    updated package-info
    Former-commit-id: 2af313130c4d4a180896eb465685413385ddbdf2

--- dependency-check-core/src/main/java/org/owasp/dependencycheck/data/nuget/package-info.java
+++ dependency-check-core/src/main/java/org/owasp/dependencycheck/data/nuget/package-info.java
@@ -1,15 +1,5 @@
 /**
- * <html>
- * <head>
- * <title>org.owasp.dependencycheck.data.nuget</title>
- * </head>
- * <body>
- * <p>
- * Contains classes related to parsing Nuget related files</p>
- * <p>
- * These are used to abstract away Nuget-related handling from Dependency Check
- * so they can be used elsewhere.</p>
- * </body>
- * </html>
+ * Contains classes related to parsing Nuget related files<br/><br/>
+ * These are used to abstract away Nuget-related handling from Dependency Check so they can be used elsewhere.
  */
 package org.owasp.dependencycheck.data.nuget;
