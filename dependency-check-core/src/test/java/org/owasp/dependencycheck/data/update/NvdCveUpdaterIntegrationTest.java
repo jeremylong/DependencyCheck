b9a53775b6ae137384b22ce0e78461e983ab7a84
Jeremy Long <jeremy.long@gmail.com>
2015-04-05 09:22:00 -0400

    updated javadoc author
    Former-commit-id: 7cd4642264106e5772b942f97bb44e4dc938ad7d

--- dependency-check-core/src/test/java/org/owasp/dependencycheck/data/update/NvdCveUpdaterIntegrationTest.java
+++ dependency-check-core/src/test/java/org/owasp/dependencycheck/data/update/NvdCveUpdaterIntegrationTest.java
@@ -26,7 +26,7 @@ import org.owasp.dependencycheck.utils.Settings;
 
 /**
  *
- * @author Jeremy Long <jeremy.long@owasp.org>
+ * @author Jeremy Long
  */
 public class NvdCveUpdaterIntegrationTest extends BaseTest {
 
