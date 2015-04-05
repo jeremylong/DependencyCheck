f367ad7185574568423ecc533837d4feb0b1d48f
Jeremy Long <jeremy.long@gmail.com>
2015-04-05 09:21:29 -0400

    updated javadoc author
    Former-commit-id: 4802729f76f07cedf9ea678822a7b884792ded9b

--- dependency-check-core/src/main/java/org/owasp/dependencycheck/data/lucene/AlphaNumericTokenizer.java
+++ dependency-check-core/src/main/java/org/owasp/dependencycheck/data/lucene/AlphaNumericTokenizer.java
@@ -24,7 +24,7 @@ import org.apache.lucene.util.Version;
 /**
  * Tokenizes the input breaking it into tokens when non-alpha/numeric characters are found.
  *
- * @author Jeremy Long <jeremy.long@owasp.org>
+ * @author Jeremy Long
  */
 public class AlphaNumericTokenizer extends CharTokenizer {
 
