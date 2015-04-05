cdbd513e42e7ba9ab7fc21a04ef785cecb0aeeaf
Jeremy Long <jeremy.long@gmail.com>
2015-04-05 09:21:18 -0400

    updated javadoc author
    Former-commit-id: b8f9f04bdb055931b1f8ba1553187ee17a5f68c3

--- dependency-check-cli/src/main/java/org/owasp/dependencycheck/App.java
+++ dependency-check-cli/src/main/java/org/owasp/dependencycheck/App.java
@@ -41,7 +41,7 @@ import org.owasp.dependencycheck.utils.Settings;
 /**
  * The command line interface for the DependencyCheck application.
  *
- * @author Jeremy Long <jeremy.long@owasp.org>
+ * @author Jeremy Long
  */
 public class App {
 
