d78e82bd567c828ae553735c41ef54c13f9ffcb5
Jeremy Long <jeremy.long@gmail.com>
2015-04-05 09:31:28 -0400

    updated package-info
    Former-commit-id: 232d2259875911ee517ed71dd741801f285a7ad4

--- dependency-check-core/src/main/java/org/owasp/dependencycheck/analyzer/exception/package-info.java
+++ dependency-check-core/src/main/java/org/owasp/dependencycheck/analyzer/exception/package-info.java
@@ -1,12 +1,4 @@
 /**
- * <html>
- * <head>
- * <title>org.owasp.dependencycheck.analyzer.exception</title>
- * </head>
- * <body>
- * <p>
- * A collection of exception classes used within the analyzers.</p>
- * </body>
- * </html>
+ * A collection of exception classes used within the analyzers.
  */
 package org.owasp.dependencycheck.analyzer.exception;
