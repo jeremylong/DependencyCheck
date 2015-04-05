0f8ce0964678f79bc07e4e78960b9cb667ee4ab6
Jeremy Long <jeremy.long@gmail.com>
2015-04-05 09:31:33 -0400

    updated package-info
    Former-commit-id: d93c76a844ab6c9177856ab74b27f8d7e9d547b1

--- dependency-check-core/src/main/java/org/owasp/dependencycheck/reporting/package-info.java
+++ dependency-check-core/src/main/java/org/owasp/dependencycheck/reporting/package-info.java
@@ -1,12 +1,4 @@
 /**
- * <html>
- * <head>
- * <title>org.owasp.dependencycheck.reporting</title>
- * </head>
- * <body>
  * Contains classes used to generate reports.
- * </body>
- * </html>
-*/
-
+ */
 package org.owasp.dependencycheck.reporting;
