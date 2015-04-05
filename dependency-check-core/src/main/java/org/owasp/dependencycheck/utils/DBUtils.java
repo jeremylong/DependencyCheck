a6014f30dd72de9002433028e54f4996eb64bfaa
Jeremy Long <jeremy.long@gmail.com>
2015-04-05 09:21:46 -0400

    updated javadoc author
    Former-commit-id: e6db05a05dddce2cc87110f0a926280a2fbcc411

--- dependency-check-core/src/main/java/org/owasp/dependencycheck/utils/DBUtils.java
+++ dependency-check-core/src/main/java/org/owasp/dependencycheck/utils/DBUtils.java
@@ -27,7 +27,7 @@ import org.owasp.dependencycheck.data.nvdcve.DatabaseException;
 
 /**
  *
- * @author Jeremy Long <jeremy.long@owasp.org>
+ * @author Jeremy Long
  */
 public final class DBUtils {
 
