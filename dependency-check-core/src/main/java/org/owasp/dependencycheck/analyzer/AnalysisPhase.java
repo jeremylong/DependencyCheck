5db51a2abfe7528d0b9148187ebeeecdb8c10719
Jeremy Long <jeremy.long@gmail.com>
2015-04-05 09:21:21 -0400

    updated javadoc author
    Former-commit-id: 8d5db6e14ce0231f572f85f580b276880964e4d9

--- dependency-check-core/src/main/java/org/owasp/dependencycheck/analyzer/AnalysisPhase.java
+++ dependency-check-core/src/main/java/org/owasp/dependencycheck/analyzer/AnalysisPhase.java
@@ -20,7 +20,7 @@ package org.owasp.dependencycheck.analyzer;
 /**
  * An enumeration defining the phases of analysis.
  *
- * @author Jeremy Long <jeremy.long@owasp.org>
+ * @author Jeremy Long
  */
 public enum AnalysisPhase {
 
