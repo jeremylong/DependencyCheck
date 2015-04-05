871e27d19fe83e28d192b2d4ed9c766bb470dd5a
Jeremy Long <jeremy.long@gmail.com>
2015-04-05 09:31:33 -0400

    updated package-info
    Former-commit-id: 3375a31fca8abb2d7553e0dfb39c96cc2bb8a44b

--- dependency-check-core/src/main/java/org/owasp/dependencycheck/package-info.java
+++ dependency-check-core/src/main/java/org/owasp/dependencycheck/package-info.java
@@ -1,12 +1,4 @@
 /**
- * <html>
- * <head>
- * <title>org.owasp.dependencycheck</title>
- * </head>
- * <body>
  * Includes the main entry point (i.e. the engine) for dependency-check.
- * </body>
- * </html>
-*/
-
+ */
 package org.owasp.dependencycheck;
