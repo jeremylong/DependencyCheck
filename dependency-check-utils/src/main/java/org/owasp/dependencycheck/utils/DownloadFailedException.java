9daa111f9c4c487ab253693a1ff4b55ab87281f0
Jeremy Long <jeremy.long@gmail.com>
2015-04-05 09:22:11 -0400

    updated javadoc author
    Former-commit-id: bb9eaba78f1575d83249b36300ea62132430b548

--- dependency-check-utils/src/main/java/org/owasp/dependencycheck/utils/DownloadFailedException.java
+++ dependency-check-utils/src/main/java/org/owasp/dependencycheck/utils/DownloadFailedException.java
@@ -22,7 +22,7 @@ import java.io.IOException;
 /**
  * An exception used when a download fails.
  *
- * @author Jeremy Long <jeremy.long@owasp.org>
+ * @author Jeremy Long
  */
 public class DownloadFailedException extends IOException {
 
