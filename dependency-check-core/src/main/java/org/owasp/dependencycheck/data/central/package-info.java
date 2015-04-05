7190dcc04eb71ac8dedaa36c8062f3670d02aca6
Jeremy Long <jeremy.long@gmail.com>
2015-04-05 09:31:29 -0400

    updated package-info
    Former-commit-id: c459f7479fc476f19ee1ba52dc162a9ead751fc8

--- dependency-check-core/src/main/java/org/owasp/dependencycheck/data/central/package-info.java
+++ dependency-check-core/src/main/java/org/owasp/dependencycheck/data/central/package-info.java
@@ -1,14 +1,7 @@
 /**
- * <html>
- * <head>
- * <title>org.owasp.dependencycheck.data.central</title>
- * </head>
- * <body>
- * <p>
- * Contains classes related to searching Maven Central.</p>
- * <p>
- * These are used to abstract Maven Central searching away from OWASP Dependency Check so they can be reused elsewhere.</p>
- * </body>
- * </html>
+ *
+ * Contains classes related to searching Maven Central.<br/><br/>
+ *
+ * These are used to abstract Maven Central searching away from OWASP Dependency Check so they can be reused elsewhere.
  */
 package org.owasp.dependencycheck.data.central;
