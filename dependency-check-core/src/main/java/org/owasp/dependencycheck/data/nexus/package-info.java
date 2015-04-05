5d647155b65d948449cb3fdccecf575404419434
Jeremy Long <jeremy.long@gmail.com>
2015-04-05 09:31:30 -0400

    updated package-info
    Former-commit-id: 9ca5a2b3bd7e86495d73c97bde84e731c22e1e61

--- dependency-check-core/src/main/java/org/owasp/dependencycheck/data/nexus/package-info.java
+++ dependency-check-core/src/main/java/org/owasp/dependencycheck/data/nexus/package-info.java
@@ -1,14 +1,6 @@
 /**
- * <html>
- * <head>
- * <title>org.owasp.dependencycheck.data.nexus</title>
- * </head>
- * <body>
- * <p>
- * Contains classes related to searching a Nexus repository.</p>
- * <p>
- * These are used to abstract Nexus searching away from OWASP Dependency Check so they can be reused elsewhere.</p>
- * </body>
- * </html>
+ * Contains classes related to searching a Nexus repository.<br/><br/>
+ *
+ * These are used to abstract Nexus searching away from OWASP Dependency Check so they can be reused elsewhere.
  */
 package org.owasp.dependencycheck.data.nexus;
