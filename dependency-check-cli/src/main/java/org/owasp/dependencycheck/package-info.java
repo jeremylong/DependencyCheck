85c3368cda49c5044bab4a40afb50bb1cca120ae
Jeremy Long <jeremy.long@gmail.com>
2015-04-05 09:31:28 -0400

    updated package-info
    Former-commit-id: e4991a34fc6959d2d7352722a8fdc8d9d9249857

--- dependency-check-cli/src/main/java/org/owasp/dependencycheck/package-info.java
+++ dependency-check-cli/src/main/java/org/owasp/dependencycheck/package-info.java
@@ -1,12 +1,4 @@
 /**
- * <html>
- * <head>
- * <title>org.owasp.dependencycheck</title>
- * </head>
- * <body>
  * Includes the main entry point for the DependencyChecker.
- * </body>
- * </html>
-*/
-
+ */
 package org.owasp.dependencycheck;
