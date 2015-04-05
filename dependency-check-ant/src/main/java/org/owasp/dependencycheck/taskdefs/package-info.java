a4095cec8d3e10e7d90d11748bb650988977a071
Jeremy Long <jeremy.long@gmail.com>
2015-04-05 09:31:27 -0400

    updated package-info
    Former-commit-id: be1142cf59d4755dc703d400d449574ca7775087

--- dependency-check-ant/src/main/java/org/owasp/dependencycheck/taskdefs/package-info.java
+++ dependency-check-ant/src/main/java/org/owasp/dependencycheck/taskdefs/package-info.java
@@ -1,11 +1,4 @@
 /**
- * <html>
- * <head>
- * <title>org.owasp.dependencycheck.taskdefs</title>
- * </head>
- * <body>
  * This package includes the Ant task definitions.
- * </body>
- * </html>
  */
 package org.owasp.dependencycheck.taskdefs;
