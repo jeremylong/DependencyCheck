8a886f5434720eec386130ae6c7353ad4e52d62e
Jeremy Long <jeremy.long@gmail.com>
2015-04-05 09:21:52 -0400

    updated javadoc author
    Former-commit-id: ebeeadb36fbe79a541a66d5b823a07c18645b6d1

--- dependency-check-core/src/test/java/org/owasp/dependencycheck/analyzer/FalsePositiveAnalyzerTest.java
+++ dependency-check-core/src/test/java/org/owasp/dependencycheck/analyzer/FalsePositiveAnalyzerTest.java
@@ -23,7 +23,7 @@ import org.owasp.dependencycheck.dependency.Dependency;
 
 /**
  *
- * @author Jeremy Long <jeremy.long@owasp.org>
+ * @author Jeremy Long
  */
 public class FalsePositiveAnalyzerTest {
 
