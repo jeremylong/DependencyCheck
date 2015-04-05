08eae2b09ca32a1ae55fd0ef62ceb9ef60446749
Jeremy Long <jeremy.long@gmail.com>
2015-04-05 09:31:31 -0400

    updated package-info
    Former-commit-id: 206b5f173ad7f2f87dc5a5ff8232a4099f07ef29

--- dependency-check-core/src/main/java/org/owasp/dependencycheck/data/update/package-info.java
+++ dependency-check-core/src/main/java/org/owasp/dependencycheck/data/update/package-info.java
@@ -1,15 +1,9 @@
 /**
- * <html>
- * <head>
- * <title>org.owasp.dependencycheck.data.update</title>
- * </head>
- * <body>
- * <p>Contains classes used to update the data stores.</p>
- * <p>The UpdateService will load, any correctly defined CachedWebDataSource(s)
- * and call update() on them. The Cached Data Source must determine if it needs
- * to be updated and if so perform the update. The sub packages contain classes
- * used to perform the actual updates.</p>
- * </body>
- * </html>
+ *
+ * Contains classes used to update the data stores.<br/><br/>
+ *
+ * The UpdateService will load, any correctly defined CachedWebDataSource(s) and call update() on them. The Cached Data Source
+ * must determine if it needs to be updated and if so perform the update. The sub packages contain classes used to perform the
+ * actual updates.
  */
 package org.owasp.dependencycheck.data.update;
