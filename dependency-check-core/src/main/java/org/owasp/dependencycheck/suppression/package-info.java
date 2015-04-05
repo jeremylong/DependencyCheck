e0994e0e54110814e85aff2e34035e18a6874941
Jeremy Long <jeremy.long@gmail.com>
2015-04-05 09:31:34 -0400

    updated package-info
    Former-commit-id: 385c57cab581f700ec110dc02245c4b37976148d

--- dependency-check-core/src/main/java/org/owasp/dependencycheck/suppression/package-info.java
+++ dependency-check-core/src/main/java/org/owasp/dependencycheck/suppression/package-info.java
@@ -1,11 +1,4 @@
 /**
- * <html>
- * <head>
- * <title>org.owasp.dependencycheck.suppression</title>
- * </head>
- * <body>
  * Contains classes used to suppress findings.
- * </body>
- * </html>
  */
 package org.owasp.dependencycheck.suppression;
