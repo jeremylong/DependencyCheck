a7ffeb7016c208746c3c3776568835aecd65c266
Jeremy Long <jeremy.long@gmail.com>
2015-04-05 09:21:59 -0400

    updated javadoc author
    Former-commit-id: e63cbab39d36b0136a819861d761f8cddc2f45c8

--- dependency-check-core/src/test/java/org/owasp/dependencycheck/data/update/EngineVersionCheckTest.java
+++ dependency-check-core/src/test/java/org/owasp/dependencycheck/data/update/EngineVersionCheckTest.java
@@ -30,7 +30,7 @@ import org.owasp.dependencycheck.utils.DependencyVersion;
 
 /**
  *
- * @author Jeremy Long <jeremy.long@owasp.org>
+ * @author Jeremy Long
  */
 public class EngineVersionCheckTest extends BaseTest {
 
