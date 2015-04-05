a7c1d594dcb0ff4f47e830048819f3e6575cbd5e
Jeremy Long <jeremy.long@gmail.com>
2015-04-05 09:31:34 -0400

    updated package-info
    Former-commit-id: 5d02d7dacbc0e20efffdaecc5a1630d102114fa5

--- dependency-check-maven/src/main/java/org/owasp/dependencycheck/maven/package-info.java
+++ dependency-check-maven/src/main/java/org/owasp/dependencycheck/maven/package-info.java
@@ -1,12 +1,4 @@
 /**
- * <html>
- * <head>
- * <title>org.owasp.dependencycheck.maven</title>
- * </head>
- * <body>
  * This is the main package containing the Mojo Maven Plugin - dependency-check.
- * </body>
- * </html>
-*/
-
+ */
 package org.owasp.dependencycheck.maven;
