c152bd051739cbae9ba675b76b799b3c5aec0949
Jeremy Long <jeremy.long@gmail.com>
2015-04-05 09:31:30 -0400

    updated package-info
    Former-commit-id: cda9f52d0d8f46523bc65f0b84ba9bc62e16b065

--- dependency-check-core/src/main/java/org/owasp/dependencycheck/data/lucene/package-info.java
+++ dependency-check-core/src/main/java/org/owasp/dependencycheck/data/lucene/package-info.java
@@ -1,12 +1,4 @@
 /**
- * <html>
- * <head>
- * <title>org.owasp.dependencycheck.data.lucene</title>
- * </head>
- * <body>
  * Contains classes used to work with the Lucene Indexes.
- * </body>
- * </html>
-*/
-
+ */
 package org.owasp.dependencycheck.data.lucene;
