f35146d93df1b12eec2537495359f6a17d6c6038
Jeremy Long <jeremy.long@gmail.com>
2015-04-05 09:31:29 -0400

    updated package-info
    Former-commit-id: 85603781da2c18a1275e13385080abcf662a331b

--- dependency-check-core/src/main/java/org/owasp/dependencycheck/data/cwe/package-info.java
+++ dependency-check-core/src/main/java/org/owasp/dependencycheck/data/cwe/package-info.java
@@ -1,12 +1,4 @@
 /**
- * <html>
- * <head>
- * <title>org.owasp.dependencycheck.data.cwe</title>
- * </head>
- * <body>
  * Contains classes for working with the CWE Database.
- * </body>
- * </html>
-*/
-
+ */
 package org.owasp.dependencycheck.data.cwe;
