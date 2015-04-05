520370688c27afece90fbb07b8e07a1603d87b05
Jeremy Long <jeremy.long@gmail.com>
2015-04-05 09:31:31 -0400

    updated package-info
    Former-commit-id: c577b32102ac872b713df1c88b2af3424f00565c

--- dependency-check-core/src/main/java/org/owasp/dependencycheck/data/nvdcve/package-info.java
+++ dependency-check-core/src/main/java/org/owasp/dependencycheck/data/nvdcve/package-info.java
@@ -1,12 +1,4 @@
 /**
- * <html>
- * <head>
- * <title>org.owasp.dependencycheck.data.nvdcve</title>
- * </head>
- * <body>
  * Contains classes used to work with the NVD CVE data.
- * </body>
- * </html>
-*/
-
+ */
 package org.owasp.dependencycheck.data.nvdcve;
