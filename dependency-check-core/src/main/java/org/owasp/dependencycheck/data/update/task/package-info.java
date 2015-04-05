03ce1d2ea8ecc61acd2e560ec02cc13b3380cb50
Jeremy Long <jeremy.long@gmail.com>
2015-04-05 09:31:31 -0400

    updated package-info
    Former-commit-id: a38dc7f72dda348947817f35ecaa0eadc39ac99c

--- dependency-check-core/src/main/java/org/owasp/dependencycheck/data/update/task/package-info.java
+++ dependency-check-core/src/main/java/org/owasp/dependencycheck/data/update/task/package-info.java
@@ -1,12 +1,4 @@
 /**
- * <html>
- * <head>
- * <title>org.owasp.dependencycheck.data.update.task</title>
- * </head>
- * <body>
- * <p>A collection of callable/runnable tasks used to speed up the update
- * process.</p>
- * </body>
- * </html>
+ * A collection of callable/runnable tasks used to speed up the update process.
  */
 package org.owasp.dependencycheck.data.update.task;
