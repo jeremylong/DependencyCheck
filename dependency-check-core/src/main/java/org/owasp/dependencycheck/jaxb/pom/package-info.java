c429df32805a87feb93a256b8f0bcfa48fadd17d
Jeremy Long <jeremy.long@gmail.com>
2015-04-05 09:31:33 -0400

    updated package-info
    Former-commit-id: 864920215f4b1620550c5bba2822d596f885d0be

--- dependency-check-core/src/main/java/org/owasp/dependencycheck/jaxb/pom/package-info.java
+++ dependency-check-core/src/main/java/org/owasp/dependencycheck/jaxb/pom/package-info.java
@@ -1,12 +1,4 @@
 /**
- * <html>
- * <head>
- * <title>org.owasp.dependencycheck.analyzer.pom</title>
- * </head>
- * <body>
  * This package contains utility classes used to parse pom.xml files.
- * </body>
- * </html>
-*/
-
+ */
 package org.owasp.dependencycheck.jaxb.pom;
