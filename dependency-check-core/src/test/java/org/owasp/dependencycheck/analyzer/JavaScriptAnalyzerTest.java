4019a0615c8c5df9f942152ddd10d93736df486d
Jeremy Long <jeremy.long@gmail.com>
2015-04-05 09:21:54 -0400

    updated javadoc author
    Former-commit-id: 04d2b404e05a3d256f5264919434f9e42c3115fb

--- dependency-check-core/src/test/java/org/owasp/dependencycheck/analyzer/JavaScriptAnalyzerTest.java
+++ dependency-check-core/src/test/java/org/owasp/dependencycheck/analyzer/JavaScriptAnalyzerTest.java
@@ -28,7 +28,7 @@ import org.owasp.dependencycheck.dependency.Dependency;
 
 /**
  *
- * @author Jeremy Long <jeremy.long@owasp.org>
+ * @author Jeremy Long
  */
 public class JavaScriptAnalyzerTest extends BaseTest {
 
