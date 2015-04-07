7bde6baaace221dc11d5e8ced3efc7bbac438f4c
Jeremy Long <jeremy.long@gmail.com>
2015-04-07 06:57:08 -0400

    replaced jaxb with sax parser
    Former-commit-id: a52e0ca02d5bb11aead699d62a38e50b1dd6670b

---
+++ dependency-check-core/src/main/java/org/owasp/dependencycheck/xml/pom/PomParseException.java
@@ -0,0 +1,64 @@
+/*
+ * This file is part of dependency-check-core.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright (c) 2015 Jeremy Long. All Rights Reserved.
+ */
+package org.owasp.dependencycheck.xml.pom;
+
+import org.owasp.dependencycheck.suppression.*;
+import java.io.IOException;
+
+/**
+ * An exception used when parsing a suppression rule file fails.
+ *
+ * @author Jeremy Long
+ */
+public class PomParseException extends IOException {
+
+    /**
+     * Creates a new SuppressionParseException.
+     */
+    public PomParseException() {
+        super();
+    }
+
+    /**
+     * Creates a new SuppressionParseException.
+     *
+     * @param msg a message for the exception.
+     */
+    public PomParseException(String msg) {
+        super(msg);
+    }
+
+    /**
+     * Creates a new SuppressionParseException.
+     *
+     * @param ex the cause of the parse exception
+     */
+    public PomParseException(Throwable ex) {
+        super(ex);
+    }
+
+    /**
+     * Creates a new SuppressionParseException.
+     *
+     * @param msg a message for the exception.
+     * @param ex the cause of the parse exception
+     */
+    public PomParseException(String msg, Throwable ex) {
+        super(msg, ex);
+    }
+}
