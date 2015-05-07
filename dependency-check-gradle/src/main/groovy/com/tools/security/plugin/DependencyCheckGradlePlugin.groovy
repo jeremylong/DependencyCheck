8fbf8df0bd2a402914badbf70264ae89439b9f10
ma wei <wma@thoughtworks.com>
2015-05-07 11:25:31 +0800

    initial check in
    Former-commit-id: 2be8565868e6c8d62f86fcd7c70d350464c667c6

---
+++ dependency-check-gradle/src/main/groovy/com/tools/security/plugin/DependencyCheckGradlePlugin.groovy
@@ -0,0 +1,16 @@
+package com.tools.security.plugin;
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+class DependencyCheckGradlePlugin implements Plugin<Project> {
+
+    private Logger logger = LoggerFactory.getLogger(getClass())
+
+    @Override
+    void apply(Project project) {
+
+    }
+}
