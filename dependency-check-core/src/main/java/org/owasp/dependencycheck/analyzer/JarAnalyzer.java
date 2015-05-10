d5885acd6eb31fd9c2bfea950e811d2e08111a21
Jeremy Long <jeremy.long@gmail.com>
2015-05-10 08:19:00 -0400

    checkstyle corrections
    Former-commit-id: 02db425ee86feb94346bfaf6e7cb8eb6b3b078b3

--- dependency-check-core/src/main/java/org/owasp/dependencycheck/analyzer/JarAnalyzer.java
+++ dependency-check-core/src/main/java/org/owasp/dependencycheck/analyzer/JarAnalyzer.java
@@ -19,15 +19,12 @@ package org.owasp.dependencycheck.analyzer;
 
 import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.Reader;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Enumeration;
@@ -46,7 +43,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Pattern;
 import java.util.zip.ZipEntry;
-import javax.xml.transform.sax.SAXSource;
 import org.jsoup.Jsoup;
 import org.owasp.dependencycheck.Engine;
 import org.owasp.dependencycheck.analyzer.exception.AnalysisException;
@@ -57,10 +53,7 @@ import org.owasp.dependencycheck.xml.pom.License;
 import org.owasp.dependencycheck.xml.pom.PomUtils;
 import org.owasp.dependencycheck.xml.pom.Model;
 import org.owasp.dependencycheck.utils.FileUtils;
-import org.owasp.dependencycheck.utils.NonClosingStream;
 import org.owasp.dependencycheck.utils.Settings;
-import org.owasp.dependencycheck.xml.pom.PomParser;
-import org.xml.sax.InputSource;
 
 /**
  * Used to load a JAR file and collect information that can be used to determine the associated CPE.
@@ -422,8 +415,7 @@ public class JarAnalyzer extends AbstractFileTypeAnalyzer {
             closeStream(fos);
             closeStream(input);
         }
-        Model model = PomUtils.readPom(file);
-        return model;
+        return PomUtils.readPom(file);
     }
 
     /**
