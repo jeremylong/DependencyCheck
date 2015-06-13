46b4761f1a5e1481a572c9c1c4e9775142d62726
Jeremy Long <jeremy.long@gmail.com>
2015-06-13 08:32:38 -0400

    updated test case initialization
    Former-commit-id: eb6c0b16c2ca4555d81d8c66db18198991ab8d51

--- dependency-check-core/src/test/java/org/owasp/dependencycheck/analyzer/ArchiveAnalyzerIntegrationTest.java
+++ dependency-check-core/src/test/java/org/owasp/dependencycheck/analyzer/ArchiveAnalyzerIntegrationTest.java
@@ -113,6 +113,8 @@ public class ArchiveAnalyzerIntegrationTest extends AbstractDatabaseTestCase {
     @Test
     public void testInitialize() throws Exception {
         ArchiveAnalyzer instance = new ArchiveAnalyzer();
+        instance.setEnabled(true);
+        instance.setFilesMatched(true);
         instance.initialize();
 
         instance.close();
