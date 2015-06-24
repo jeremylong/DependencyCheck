e6707c65a57c200f3d97f6e9af7e83ac9b062471
Dale Visser <dvisser@ida.org>
2015-06-24 15:25:03 -0400

    Made magic number constants private.
    Former-commit-id: 12539d50efb17790b770934d10b953e0fd180c8a

--- dependency-check-core/src/main/java/org/owasp/dependencycheck/dependency/Evidence.java
+++ dependency-check-core/src/main/java/org/owasp/dependencycheck/dependency/Evidence.java
@@ -29,8 +29,15 @@ import java.io.Serializable;
  */
 public class Evidence implements Serializable, Comparable<Evidence> {
 
-    public static final int MAGIC_HASH_INIT_VALUE = 3;
-    public static final int MAGIC_HASH_MULTIPLIER = 67;
+    /**
+     * Used as starting point for generating the value in {@link #hashCode()}.
+     */
+    private static final int MAGIC_HASH_INIT_VALUE = 3;
+
+    /**
+     * Used as a multiplier for generating the value in {@link #hashCode()}.
+     */
+    private static final int MAGIC_HASH_MULTIPLIER = 67;
 
     /**
      * Creates a new Evidence object.
