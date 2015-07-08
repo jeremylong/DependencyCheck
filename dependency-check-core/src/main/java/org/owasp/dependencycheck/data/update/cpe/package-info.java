db42da14d1a27e564bbb9eb38746c95f4dee7fc1
Jeremy Long <jeremy.long@gmail.com>
2015-07-08 06:31:08 -0400

    updated documentation
    Former-commit-id: f4b200a38c5df345cd5714c4dbf624af57b40481

---
+++ dependency-check-core/src/main/java/org/owasp/dependencycheck/data/update/cpe/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Contains classes used to parse the CPE XML file from NIST.<br/><br/>
+ *
+ * These classes are not used as they add no value over the existing CPE data contained within the CVE data from the NVD. However,
+ * we may consider pulling the more descriptive data from the CPE data in the future.
+ */
+package org.owasp.dependencycheck.data.update.cpe;
--- dependency-check-core/src/main/java/org/owasp/dependencycheck/data/update/nvd/package-info.java
+++ dependency-check-core/src/main/java/org/owasp/dependencycheck/data/update/nvd/package-info.java
@@ -1,8 +1,4 @@
 /**
- * Contains classes used to parse the NVD CVE XML file.<br/><br/>
- *
- * The basic use is that the Importer is called to import an NVD CVE file. The Importer instantiates an Indexer object (which
- * extends Index). The Indexer creates a partial-unmarshalling SAX parser (implemented in the NvdCveXmlFilter) that extracts
- * VulnerabilityTypes (aka Entry) from the NVD CVE data file and stores these into a Lucene Index.
+ * Contains classes used to download, parse, and load the NVD CVE data from NIST into the local database.<br/><br/>
  */
 package org.owasp.dependencycheck.data.update.nvd;
