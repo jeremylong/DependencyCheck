bf3fe6404a7dda7cce2b88de6292fe897f4fe1eb
Jeremy Long <jeremy.long@gmail.com>
2015-07-09 06:18:30 -0400

    reverted changes as a schema upgrade is not needed at this time
    Former-commit-id: 22671cb1c7e2e6db26f704f12b5fb86100586221

--- dependency-check-core/src/main/resources/data/upgrade_2.9.sql
+++ dependency-check-core/src/main/resources/data/upgrade_2.9.sql
@@ -1,6 +1,7 @@
 
-ALTER TABLE cpeEntry ADD COLUMN IF NOT EXISTS dictionaryEntry BOOLEAN;
-ALTER TABLE cpeEntry ALTER COLUMN dictionaryEntry  SET DEFAULT FALSE;
-UPDATE cpeEntry SET dictionaryEntry=false;
+--the following is not currently used.
+--ALTER TABLE cpeEntry ADD COLUMN IF NOT EXISTS dictionaryEntry BOOLEAN;
+--ALTER TABLE cpeEntry ALTER COLUMN dictionaryEntry  SET DEFAULT FALSE;
+--UPDATE cpeEntry SET dictionaryEntry=false;
 
-UPDATE Properties SET value='3.0' WHERE ID='version';
+--UPDATE Properties SET value='3.0' WHERE ID='version';
