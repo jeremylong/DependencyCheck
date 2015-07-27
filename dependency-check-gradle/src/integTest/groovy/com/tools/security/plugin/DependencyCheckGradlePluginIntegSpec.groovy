cafa0d657800d51da0e5f68cfcbd791724b457f4
Sion Williams <sion5@hotmail.co.uk>
2015-07-27 22:03:19 +0100

    Integration test spec proves outputDirectory value is not being honoured when changed using dsl.

---
+++ dependency-check-gradle/src/integTest/groovy/com/tools/security/plugin/DependencyCheckGradlePluginIntegSpec.groovy
@@ -0,0 +1,34 @@
+package com.tools.security.plugin
+
+import nebula.test.IntegrationSpec
+import nebula.test.functional.ExecutionResult
+
+/**
+ * @author Sion Williams
+ */
+class DependencyCheckGradlePluginIntegSpec extends IntegrationSpec {
+    def "I can add the plugin to a build with no errors"() {
+        setup:
+        buildFile << '''
+            apply plugin: 'dependency-check'
+        '''.stripIndent()
+
+        when:
+        ExecutionResult result = runTasksSuccessfully('tasks')
+
+        then:
+        result.standardOutput.contains('dependencyCheck - Produce dependency security report.')
+    }
+
+    def "I can override outputDir with extension"() {
+        setup:
+        writeHelloWorld('com.example')
+        copyResources('outputDir.gradle', 'build.gradle')
+
+        when:
+        runTasksSuccessfully('dependencyCheck')
+
+        then:
+        fileExists('build/dependencyCheckReport')
+    }
+}
