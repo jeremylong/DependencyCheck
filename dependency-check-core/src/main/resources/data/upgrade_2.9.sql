6949b3c229eb468b1b41c19b56fb9a7837f577d1
Jeremy Long <jeremy.long@gmail.com>
2015-10-26 06:30:56 -0400

    update file cannot contain comments

--- dependency-check-core/src/main/resources/data/upgrade_2.9.sql
+++ dependency-check-core/src/main/resources/data/upgrade_2.9.sql
@@ -1,7 +1,1 @@
-
---the following is not currently used.
---ALTER TABLE cpeEntry ADD COLUMN IF NOT EXISTS dictionaryEntry BOOLEAN;
---ALTER TABLE cpeEntry ALTER COLUMN dictionaryEntry  SET DEFAULT FALSE;
---UPDATE cpeEntry SET dictionaryEntry=false;
-
 UPDATE Properties SET value='3.0' WHERE ID='version';
