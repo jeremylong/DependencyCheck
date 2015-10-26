08d7b3dbce05671ff020de096b2087b213e93439
Jeremy Long <jeremy.long@gmail.com>
2015-10-26 06:31:43 -0400

    currently unused but contains DDL that may be needed in the future

---
+++ dependency-check-core/src/main/resources/data/upgrade_3.0.sql
@@ -0,0 +1,7 @@
+
+--the following is not currently used.
+--ALTER TABLE cpeEntry ADD COLUMN IF NOT EXISTS dictionaryEntry BOOLEAN;
+--ALTER TABLE cpeEntry ALTER COLUMN dictionaryEntry  SET DEFAULT FALSE;
+--UPDATE cpeEntry SET dictionaryEntry=false;
+
+--UPDATE Properties SET value='3.1' WHERE ID='version';
