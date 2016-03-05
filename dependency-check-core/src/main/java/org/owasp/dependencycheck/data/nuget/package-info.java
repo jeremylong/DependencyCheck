28840c6209b5157a6d21d61b3d04bca9a1832595
Jeremy Long <jeremy.long@gmail.com>
2016-03-05 13:18:29 -0500

    doclint fixes

--- dependency-check-core/src/main/java/org/owasp/dependencycheck/data/nuget/package-info.java
+++ dependency-check-core/src/main/java/org/owasp/dependencycheck/data/nuget/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Contains classes related to parsing Nuget related files<br/><br/>
+ * Contains classes related to parsing Nuget related files<br><br>
  * These are used to abstract away Nuget-related handling from Dependency Check so they can be used elsewhere.
  */
 package org.owasp.dependencycheck.data.nuget;
