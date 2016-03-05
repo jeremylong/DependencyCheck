1696213406641fb02ba74198f68c044e39b9172d
Jeremy Long <jeremy.long@gmail.com>
2016-03-05 13:18:29 -0500

    doclint fixes

--- dependency-check-core/src/main/java/org/owasp/dependencycheck/data/nexus/package-info.java
+++ dependency-check-core/src/main/java/org/owasp/dependencycheck/data/nexus/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Contains classes related to searching a Nexus repository.<br/><br/>
+ * Contains classes related to searching a Nexus repository.<br><br>
  *
  * These are used to abstract Nexus searching away from OWASP Dependency Check so they can be reused elsewhere.
  */
