24637f496f9c5eeb6985b61a446962417b50d74c
Jeremy Long <jeremy.long@gmail.com>
2016-03-05 13:18:30 -0500

    doclint fixes

--- dependency-check-core/src/main/java/org/owasp/dependencycheck/data/update/nvd/package-info.java
+++ dependency-check-core/src/main/java/org/owasp/dependencycheck/data/update/nvd/package-info.java
@@ -1,4 +1,4 @@
 /**
- * Contains classes used to download, parse, and load the NVD CVE data from NIST into the local database.<br/><br/>
+ * Contains classes used to download, parse, and load the NVD CVE data from NIST into the local database.<br><br>
  */
 package org.owasp.dependencycheck.data.update.nvd;
