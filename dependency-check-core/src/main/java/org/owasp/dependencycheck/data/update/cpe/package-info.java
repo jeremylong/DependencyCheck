d8ecde52656202f9a116784b60885ba3264da2b7
Jeremy Long <jeremy.long@gmail.com>
2016-03-05 13:18:30 -0500

    doclint fixes

--- dependency-check-core/src/main/java/org/owasp/dependencycheck/data/update/cpe/package-info.java
+++ dependency-check-core/src/main/java/org/owasp/dependencycheck/data/update/cpe/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Contains classes used to parse the CPE XML file from NIST.<br/><br/>
+ * Contains classes used to parse the CPE XML file from NIST.<br><br>
  *
  * These classes are not used as they add no value over the existing CPE data contained within the CVE data from the NVD. However,
  * we may consider pulling the more descriptive data from the CPE data in the future.
