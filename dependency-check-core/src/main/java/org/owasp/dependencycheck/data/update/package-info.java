7214b243572b48115c2b676d9fc7b66a046ee736
Jeremy Long <jeremy.long@gmail.com>
2016-03-05 13:18:31 -0500

    doclint fixes

--- dependency-check-core/src/main/java/org/owasp/dependencycheck/data/update/package-info.java
+++ dependency-check-core/src/main/java/org/owasp/dependencycheck/data/update/package-info.java
@@ -1,6 +1,6 @@
 /**
  *
- * Contains classes used to update the data stores.<br/><br/>
+ * Contains classes used to update the data stores.<br><br>
  *
  * The UpdateService will load, any correctly defined CachedWebDataSource(s) and call update() on them. The Cached Data Source
  * must determine if it needs to be updated and if so perform the update. The sub packages contain classes used to perform the
