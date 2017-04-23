865db1b6c3d4118ff47b38013048998759d3e096
Jeremy Long <jeremy.long@gmail.com>
2017-04-23 08:04:54 -0400

    nop

--- dependency-check-core/src/main/java/org/owasp/dependencycheck/data/update/NvdCveUpdater.java
+++ dependency-check-core/src/main/java/org/owasp/dependencycheck/data/update/NvdCveUpdater.java
@@ -140,6 +140,7 @@ public class NvdCveUpdater implements CachedWebDataSource {
                     throw new UpdateException("Unable to obtain the update lock, skipping the database update. Skippinig the database update.");
                 }
             }
+            
             initializeExecutorServices();
             cveDb = CveDB.getInstance();
             dbProperties = cveDb.getDatabaseProperties();
