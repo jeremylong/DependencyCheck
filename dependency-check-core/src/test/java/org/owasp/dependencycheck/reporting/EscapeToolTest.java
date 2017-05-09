ee72f172d2ded278f71002102bc032ce9294275d
Jeremy Long <jeremy.long@gmail.com>
2017-05-08 21:41:05 -0400

    fix codacy issue

--- dependency-check-core/src/test/java/org/owasp/dependencycheck/reporting/EscapeToolTest.java
+++ dependency-check-core/src/test/java/org/owasp/dependencycheck/reporting/EscapeToolTest.java
@@ -19,10 +19,6 @@ package org.owasp.dependencycheck.reporting;
 
 import java.util.HashSet;
 import java.util.Set;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.owasp.dependencycheck.dependency.Identifier;
