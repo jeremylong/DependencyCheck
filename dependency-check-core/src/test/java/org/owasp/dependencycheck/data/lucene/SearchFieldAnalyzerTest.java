795de6f9ea067a6eeace9eff7d6c22495db3fb69
Jeremy Long <jeremy.long@gmail.com>
2017-09-02 06:35:17 -0400

    added test case

---
+++ dependency-check-core/src/test/java/org/owasp/dependencycheck/data/lucene/SearchFieldAnalyzerTest.java
@@ -0,0 +1,45 @@
+/*
+ * This file is part of dependency-check-core.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright (c) 2017 Jeremy Long. All Rights Reserved.
+ */
+package org.owasp.dependencycheck.data.lucene;
+
+import java.io.Reader;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.util.CharArraySet;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author jeremy long
+ */
+public class SearchFieldAnalyzerTest {
+
+    /**
+     * Test of getStopWords method, of class SearchFieldAnalyzer.
+     */
+    @Test
+    public void testGetStopWords() {
+        CharArraySet result = SearchFieldAnalyzer.getStopWords();
+        assertTrue(result.size() > 20);
+        assertTrue(result.contains("software"));
+    }
+}
