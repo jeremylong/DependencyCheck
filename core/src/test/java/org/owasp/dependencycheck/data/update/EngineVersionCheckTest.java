3b2e0ca3c6c0218cbf38996cea92f196c157889a
Jeremy Long <jeremy.long@gmail.com>
2018-03-28 06:40:31 -0400

    removed deprecated jmockit method

--- core/src/test/java/org/owasp/dependencycheck/data/update/EngineVersionCheckTest.java
+++ core/src/test/java/org/owasp/dependencycheck/data/update/EngineVersionCheckTest.java
@@ -16,13 +16,18 @@
 package org.owasp.dependencycheck.data.update;
 
 import java.util.Properties;
+import mockit.Injectable;
 import mockit.Mock;
 import mockit.MockUp;
+import mockit.Tested;
+import mockit.integration.junit4.JMockit;
 import org.joda.time.DateTime;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.owasp.dependencycheck.BaseTest;
+import org.owasp.dependencycheck.data.nvdcve.CveDB;
 import org.owasp.dependencycheck.data.nvdcve.DatabaseProperties;
 import org.owasp.dependencycheck.data.update.exception.UpdateException;
 import org.owasp.dependencycheck.utils.DependencyVersion;
@@ -31,16 +36,117 @@ import org.owasp.dependencycheck.utils.DependencyVersion;
  *
  * @author Jeremy Long
  */
+@RunWith(JMockit.class)
 public class EngineVersionCheckTest extends BaseTest {
 
-//    /**
-//     * Test of update method, of class EngineVersionCheck.
-//     */
-//    @Test
-//    public void testUpdate() throws Exception {
-//        EngineVersionCheck instance = new EngineVersionCheck();
-//        instance.update();
-//    }
+    @Injectable
+    private CveDB cveDb;
+    @Tested
+    private DatabaseProperties dbProperties;
+
+    /**
+     * Test of shouldUpdate method, of class EngineVersionCheck.
+     */
+    @Test
+    public void testShouldUpdate() throws Exception {
+        new MockUp<DatabaseProperties>() {
+            private final Properties properties = new Properties();
+
+            @Mock
+            public void $init(CveDB db) {
+            }
+
+            @Mock
+            public void save(String key, String value) throws UpdateException {
+                properties.setProperty(key, value);
+            }
+
+            @Mock
+            public String getProperty(String key) {
+                return properties.getProperty(key);
+            }
+        };
+
+        String updateToVersion = "1.2.6";
+        String currentVersion = "1.2.6";
+
+        long lastChecked = dateToMilliseconds("2014-12-01");
+        long now = dateToMilliseconds("2014-12-01");
+
+        EngineVersionCheck instance = new EngineVersionCheck(getSettings());
+        boolean expResult = false;
+        instance.setUpdateToVersion(updateToVersion);
+        boolean result = instance.shouldUpdate(lastChecked, now, dbProperties, currentVersion);
+        assertEquals(expResult, result);
+
+        updateToVersion = "1.2.5";
+        currentVersion = "1.2.5";
+        lastChecked = dateToMilliseconds("2014-10-01");
+        now = dateToMilliseconds("2014-12-01");
+        expResult = true;
+        instance.setUpdateToVersion(updateToVersion);
+        result = instance.shouldUpdate(lastChecked, now, dbProperties, currentVersion);
+        assertEquals(expResult, result);
+        //System.out.println(properties.getProperty(CURRENT_ENGINE_RELEASE));
+
+        updateToVersion = "1.2.5";
+        currentVersion = "1.2.5";
+        lastChecked = dateToMilliseconds("2014-12-01");
+        now = dateToMilliseconds("2014-12-03");
+        expResult = false;
+        instance.setUpdateToVersion(updateToVersion);
+        result = instance.shouldUpdate(lastChecked, now, dbProperties, currentVersion);
+        assertEquals(expResult, result);
+
+        updateToVersion = "1.2.6";
+        currentVersion = "1.2.5";
+        lastChecked = dateToMilliseconds("2014-12-01");
+        now = dateToMilliseconds("2014-12-03");
+        expResult = true;
+        instance.setUpdateToVersion(updateToVersion);
+        result = instance.shouldUpdate(lastChecked, now, dbProperties, currentVersion);
+        assertEquals(expResult, result);
+
+        updateToVersion = "1.2.5";
+        currentVersion = "1.2.6";
+        lastChecked = dateToMilliseconds("2014-12-01");
+        now = dateToMilliseconds("2014-12-08");
+        expResult = false;
+        instance.setUpdateToVersion(updateToVersion);
+        result = instance.shouldUpdate(lastChecked, now, dbProperties, currentVersion);
+        assertEquals(expResult, result);
+
+        updateToVersion = "";
+        currentVersion = "1.2.5";
+        lastChecked = dateToMilliseconds("2014-12-01");
+        now = dateToMilliseconds("2014-12-03");
+        expResult = false;
+        instance.setUpdateToVersion(updateToVersion);
+        result = instance.shouldUpdate(lastChecked, now, dbProperties, currentVersion);
+        assertEquals(expResult, result);
+
+        updateToVersion = "";
+        currentVersion = "1.2.5";
+        lastChecked = dateToMilliseconds("2014-12-01");
+        now = dateToMilliseconds("2015-12-08");
+        expResult = true;
+        instance.setUpdateToVersion(updateToVersion);
+        result = instance.shouldUpdate(lastChecked, now, dbProperties, currentVersion);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getCurrentReleaseVersion method, of class EngineVersionCheck.
+     */
+    @Test
+    public void testGetCurrentReleaseVersion() {
+        EngineVersionCheck instance = new EngineVersionCheck(getSettings());
+        DependencyVersion minExpResult = new DependencyVersion("1.2.6");
+        String release = instance.getCurrentReleaseVersion();
+        DependencyVersion result = new DependencyVersion(release);
+        assertTrue(minExpResult.compareTo(result) <= 0);
+    }
+
     /**
      * Converts a date in the form of yyyy-MM-dd into the epoch milliseconds.
      *
@@ -59,103 +165,4 @@ public class EngineVersionCheckTest extends BaseTest {
         return dt.toInstant().getMillis();
     }
 
-    /**
-     * Test of shouldUpdate method, of class EngineVersionCheck.
-     */
-    @Test
-    public void testShouldUpdate() throws Exception {
-        DatabaseProperties properties = new MockUp<DatabaseProperties>() {
-            final private Properties properties = new Properties();
-            
-            @Mock
-            public void save(String key, String value) throws UpdateException {
-                properties.setProperty(key, value);
-            }
-            
-            @Mock
-            public String getProperty(String key) {
-                return properties.getProperty(key);
-            }
-            
-        }.getMockInstance();
-        
-        String updateToVersion = "1.2.6";
-        String currentVersion = "1.2.6";
-        
-        long lastChecked = dateToMilliseconds("2014-12-01");
-        long now = dateToMilliseconds("2014-12-01");
-        
-        EngineVersionCheck instance = new EngineVersionCheck(getSettings());
-        boolean expResult = false;
-        instance.setUpdateToVersion(updateToVersion);
-        boolean result = instance.shouldUpdate(lastChecked, now, properties, currentVersion);
-        assertEquals(expResult, result);
-        
-        updateToVersion = "1.2.5";
-        currentVersion = "1.2.5";
-        lastChecked = dateToMilliseconds("2014-10-01");
-        now = dateToMilliseconds("2014-12-01");
-        expResult = true;
-        instance.setUpdateToVersion(updateToVersion);
-        result = instance.shouldUpdate(lastChecked, now, properties, currentVersion);
-        assertEquals(expResult, result);
-        //System.out.println(properties.getProperty(CURRENT_ENGINE_RELEASE));
-
-        updateToVersion = "1.2.5";
-        currentVersion = "1.2.5";
-        lastChecked = dateToMilliseconds("2014-12-01");
-        now = dateToMilliseconds("2014-12-03");
-        expResult = false;
-        instance.setUpdateToVersion(updateToVersion);
-        result = instance.shouldUpdate(lastChecked, now, properties, currentVersion);
-        assertEquals(expResult, result);
-        
-        updateToVersion = "1.2.6";
-        currentVersion = "1.2.5";
-        lastChecked = dateToMilliseconds("2014-12-01");
-        now = dateToMilliseconds("2014-12-03");
-        expResult = true;
-        instance.setUpdateToVersion(updateToVersion);
-        result = instance.shouldUpdate(lastChecked, now, properties, currentVersion);
-        assertEquals(expResult, result);
-        
-        updateToVersion = "1.2.5";
-        currentVersion = "1.2.6";
-        lastChecked = dateToMilliseconds("2014-12-01");
-        now = dateToMilliseconds("2014-12-08");
-        expResult = false;
-        instance.setUpdateToVersion(updateToVersion);
-        result = instance.shouldUpdate(lastChecked, now, properties, currentVersion);
-        assertEquals(expResult, result);
-        
-        updateToVersion = "";
-        currentVersion = "1.2.5";
-        lastChecked = dateToMilliseconds("2014-12-01");
-        now = dateToMilliseconds("2014-12-03");
-        expResult = false;
-        instance.setUpdateToVersion(updateToVersion);
-        result = instance.shouldUpdate(lastChecked, now, properties, currentVersion);
-        assertEquals(expResult, result);
-        
-        updateToVersion = "";
-        currentVersion = "1.2.5";
-        lastChecked = dateToMilliseconds("2014-12-01");
-        now = dateToMilliseconds("2015-12-08");
-        expResult = true;
-        instance.setUpdateToVersion(updateToVersion);
-        result = instance.shouldUpdate(lastChecked, now, properties, currentVersion);
-        assertEquals(expResult, result);
-    }
-
-    /**
-     * Test of getCurrentReleaseVersion method, of class EngineVersionCheck.
-     */
-    @Test
-    public void testGetCurrentReleaseVersion() {
-        EngineVersionCheck instance = new EngineVersionCheck(getSettings());
-        DependencyVersion minExpResult = new DependencyVersion("1.2.6");
-        String release = instance.getCurrentReleaseVersion();
-        DependencyVersion result = new DependencyVersion(release);
-        assertTrue(minExpResult.compareTo(result) <= 0);
-    }
 }
