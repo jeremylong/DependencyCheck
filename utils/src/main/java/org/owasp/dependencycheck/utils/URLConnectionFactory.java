f3addf6ee72e706d87100ff66c835967d6ed84a1
Jeremy Long <jeremy.long@gmail.com>
2018-05-16 06:29:37 -0400

    checkstyle corrections

--- utils/src/main/java/org/owasp/dependencycheck/utils/URLConnectionFactory.java
+++ utils/src/main/java/org/owasp/dependencycheck/utils/URLConnectionFactory.java
@@ -123,7 +123,8 @@ public final class URLConnectionFactory {
             }
             throw new URLConnectionFailureException("Error getting connection.", ex);
         }
-        //conn.setRequestProperty("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36");
+        //conn.setRequestProperty("user-agent", 
+        //  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36");
         configureTLS(url, conn);
         return conn;
     }
