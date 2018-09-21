af56221d9dd896d6216a84220d0d214bbb281687
Akhilesh Tyagi <akhilesh.tyagi@microfocus.com>
2018-09-21 08:28:37 -0400

    Revert "Redid the logic."
    This reverts commit 7ce9b9bcd61a3d9e558f972930da27d22e9930f6.

--- maven/src/main/java/org/owasp/dependencycheck/maven/BaseDependencyCheckMojo.java
+++ maven/src/main/java/org/owasp/dependencycheck/maven/BaseDependencyCheckMojo.java
@@ -1572,14 +1572,27 @@ public abstract class BaseDependencyCheckMojo extends AbstractMojo implements Ma
                         }
                     }
                 } else {
+                    Proxy httpsProxy = null;
+                    Proxy httpProxy = null;
+
                     for (Proxy aProxy: proxies) {
-                        if (aProxy.getProtocol().equalsIgnoreCase("https") && aProxy.isActive()) {
-                            return aProxy;
+                        if (aProxy.getProtocol().equalsIgnoreCase("https") && aProxy.isActive() && httpsProxy == null) {
+                            httpsProxy = aProxy;
                         }
 
-                        if (aProxy.getProtocol().equalsIgnoreCase("http") && aProxy.isActive()) {
-                            return aProxy;
+                        if (aProxy.getProtocol().equalsIgnoreCase("http") && aProxy.isActive() && httpProxy == null) {
+                            httpProxy = aProxy;
                         }
+
+                        if (httpProxy != null && httpsProxy != null) {
+                            break;
+                        }
+                    }
+
+                    if (httpsProxy != null) {
+                        return httpsProxy;
+                    } else if (httpProxy != null) {
+                        return httpProxy;
                     }
                 }
             }
