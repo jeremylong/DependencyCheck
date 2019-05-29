11f7ebb811467f5d697ffa484b1f087c5938295f
Jeremy Long <jeremy.long@gmail.com>
2019-05-29 07:25:27 -0400

    fix test case

--- core/src/test/java/org/owasp/dependencycheck/data/cpe/CpeMemoryIndexTest.java
+++ core/src/test/java/org/owasp/dependencycheck/data/cpe/CpeMemoryIndexTest.java
@@ -101,7 +101,7 @@ public class CpeMemoryIndexTest extends BaseDBTestCase {
         instance.resetAnalyzers();
         searchString = "product:(struts2\\-core^2 struts^3 core) AND vendor:(apache.struts apache^3 foundation)";
 
-        expResult = "+((product:struts product:strutsstruts2 product:struts2 product:struts2core product:core)^2.0 (product:corestruts product:struts)^3.0 (product:strutscore product:core)) +((vendor:apache vendor:apachestruts vendor:struts) (vendor:strutsapache vendor:apache)^3.0)";
+        expResult = "+((product:struts2 product:struts2struts product:struts product:strutscore product:core)^2.0 (product:corestruts product:struts)^3.0 (product:strutscore product:core)) +((vendor:apache vendor:apachestruts vendor:struts) (vendor:strutsapache vendor:apache)^3.0)";
         result = instance.parseQuery(searchString);
         assertEquals(expResult, result.toString());
         instance.close();
