e57457f276cb95a09b3bc4495ecddbc4cdfb46cc
Jeremy Long <jeremy.long@gmail.com>
2019-12-03 21:21:42 -0500

    updated per #2347

--- core/src/main/java/org/owasp/dependencycheck/xml/hints/HintParser.java
+++ core/src/main/java/org/owasp/dependencycheck/xml/hints/HintParser.java
@@ -82,6 +82,11 @@ public class HintParser {
      * The schema for the hint XML files.
      */
     private static final String HINT_SCHEMA_1_3 = "schema/dependency-hint.1.3.xsd";
+    
+        /**
+     * The schema for the hint XML files.
+     */
+    private static final String HINT_SCHEMA_1_4 = "schema/dependency-hint.1.4.xsd";
 
     /**
      * The hint rules.
@@ -137,6 +142,7 @@ public class HintParser {
      */
     public void parseHints(InputStream inputStream) throws HintParseException, SAXException {
         try (
+                InputStream schemaStream14 = FileUtils.getResourceAsStream(HINT_SCHEMA_1_4);
                 InputStream schemaStream13 = FileUtils.getResourceAsStream(HINT_SCHEMA_1_3);
                 InputStream schemaStream12 = FileUtils.getResourceAsStream(HINT_SCHEMA_1_2);
                 InputStream schemaStream11 = FileUtils.getResourceAsStream(HINT_SCHEMA_1_1)) {
