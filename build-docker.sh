51659758aa54b56d7f767f6be8c1975ce65c48f4
Jeremy Long <jeremy.long@gmail.com>
2019-12-05 06:10:44 -0500

    improve docker image testing

---
+++ build-docker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+VERSION=$(mvn -q \
+    -Dexec.executable="echo" \
+    -Dexec.args='${project.version}' \
+    --non-recursive \
+    org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
+
+FILE=./cli/target/dependency-check-$VERSION-release.zip
+if [ -f "$FILE" ]; then
+    cd cli
+    docker build . --build-arg VERSION=$VERSION -t owasp/dependency-check:$VERSION
+    if [[ ! $VERSION = *"SNAPSHOT"* ]]; then
+        docker tag owasp/dependency-check:$VERSION owasp/dependency-check:latest
+    fi
+    cd ..
+else 
+    echo "$FILE does not exist - run 'mvn package' first"
+    exit 1
+fi
---
+++ publish-docker.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+
+VERSION=$(mvn -q \
+    -Dexec.executable="echo" \
+    -Dexec.args='${project.version}' \
+    --non-recursive \
+    org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
+
+if [[ $VERSION = *"SNAPSHOT"* ]]; then
+  echo "Do not publish a snapshot version of dependency-check"
+  exit 1
+fi
+docker inspect --type=image owasp/dependency-check:$VERSION  > /dev/null 2>&1
+if [[ "$?" -ne 0 ]] ; then echo woot; fi
+  echo "docker image owasp/dependency-check:$VERSION does not exist - run build_docker.sh first"
+  exit 1
+fi
+docker inspect --type=image owasp/dependency-check:latest  > /dev/null 2>&1
+if [[ "$?" -ne 0 ]] ; then echo woot; fi
+  echo "docker image owasp/dependency-check:latest does not exist - run build_docker.sh first"
+  exit 1
+fi
+
+docker push owasp/dependency-check:$VERSION 
+docker push owasp/dependency-check:latest
