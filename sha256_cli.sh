db33d8c366873b844f57de39e38243f64a31869d
Jeremy Long <jeremy.long@gmail.com>
2021-03-31 08:33:27 -0400

    update

--- sha256_cli.sh
+++ sha256_cli.sh
@@ -2,4 +2,6 @@
 
 ver=`curl -s https://jeremylong.github.io/DependencyCheck/current.txt`
 echo "Version $ver"
-curl -s https://github.com/jeremylong/DependencyCheck/releases/download/v4ver/dependency-check-$ver-release.zip | shasum -a 256
+wget -q https://github.com/jeremylong/DependencyCheck/releases/download/v$ver/dependency-check-$ver-release.zip 
+shasum -a 256 dependency-check-$ver-release.zip
+rm dependency-check-$ver-release.zip
