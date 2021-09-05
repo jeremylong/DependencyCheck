f4722095306b02deade6bd047c2359bad6822348
Nicolas Humblot <contact@nicolashumblot.fr>
2021-09-05 21:08:53 +0200

    Fix codacy issue

--- sha256_cli.sh
+++ sha256_cli.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-ver=`curl -s https://jeremylong.github.io/DependencyCheck/current.txt`
+ver=$(curl -s https://jeremylong.github.io/DependencyCheck/current.txt)
 echo "Version $ver"
 wget -q https://github.com/jeremylong/DependencyCheck/releases/download/v$ver/dependency-check-$ver-release.zip 
 shasum -a 256 dependency-check-$ver-release.zip
