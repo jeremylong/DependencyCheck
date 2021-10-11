994f1e281905c874e34cf2e023366fa5064f2cca
Jeremy Long <jeremy.long@gmail.com>
2021-10-11 14:17:29 -0400

    make checkstyle happier

--- core/src/main/java/org/owasp/dependencycheck/data/update/NvdCveUpdater.java
+++ core/src/main/java/org/owasp/dependencycheck/data/update/NvdCveUpdater.java
@@ -22,8 +22,6 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
