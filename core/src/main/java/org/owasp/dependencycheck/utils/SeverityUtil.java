df282a619c8604bf64150a574d02136adf036b65
Hans Aikema <aikebah-github@aikebah.net>
2022-03-30 18:04:52 +0200

    Rework the sorting in an alternative way that does support stable sorting regardless of the sequence in which items are entered into a sorted collection.

--- core/src/main/java/org/owasp/dependencycheck/utils/SeverityUtil.java
+++ core/src/main/java/org/owasp/dependencycheck/utils/SeverityUtil.java
@@ -17,12 +17,20 @@
  */
 package org.owasp.dependencycheck.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Utility to estimate severity level scores.
  *
  * @author Jeremy Long
  */
 public final class SeverityUtil {
+
+    /**
+     * The logger.
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(SeverityUtil.class);
 
     /**
      * Private constructor for a utility class.
@@ -55,6 +63,105 @@ public final class SeverityUtil {
             case "none":
             default:
                 return 3.9f;
+        }
+    }
+
+    public static String unscoredToSeveritytext(final String severity) {
+        switch (Severity.forUnscored(severity)) {
+            case CRITICAL:
+            case HIGH:
+            case MEDIUM:
+            case LOW:
+            case INFO:
+                return severity;
+            case ASSUMED_CRITICAL:
+            default:
+                final String sevText;
+                if ("0.0".equals(severity)) {
+                    sevText = "Unknown";
+                } else {
+                    sevText = severity;
+                }
+                return sevText + " (not recognized; assumed to be critical)";
+        }
+    }
+
+    /**
+     * Creates an estimated sort-adjusted CVSSv3 score for an unscored textual severity.
+     * For recognized severities below critical it returns a value at the lower bound of the CVSSv3 baseScore for that severity.
+     * For recognized critical severities it returns a score in-between the upper bound of the HIGH CVSSv2 score and the lowest
+     * sort-adjusted CVSSv3 critical score, so that unscored critical vulnerabilties are ordered in between CRITICAL scored CVSSv3
+     * rated vulnerabilities and HIGH-scored CVSSv2 rated vulnerabilities.
+     * For unrecognized severities it returns a score in-between the top HIGH CVSSv2 score and the estimatedSortAdjustedCVSSv3
+     * score for an unscored severity recognized as critical, so that recognized critical will win over unrecognized severities
+     * while unrecognized severities are assumed to be of a critical nature.
+     *
+     * @param severity The textual severity, may be null
+     * @return A float that can be used to numerically sort vulnerabilities in approximated severity (highest float represents
+     * highest severity).
+     * @see #sortAdjustedCVSSv3BaseScore(float)
+     */
+    public static float estimatedSortAdjustedCVSSv3(final String severity) {
+        switch (Severity.forUnscored(severity)) {
+            case CRITICAL:
+                return 10.2f;
+            case HIGH:
+                return 7.0f;
+            case MEDIUM:
+                return 4.0f;
+            case LOW:
+                return 0.1f;
+            case INFO:
+                return 0.0f;
+            case ASSUMED_CRITICAL:
+            default:
+                SeverityUtil.LOGGER.debug("Unrecognized unscored textual severity: {}, assuming critical score as worst-case "
+                                           + "estimate for sorting",
+                                           severity);
+                return 10.1f;
+        }
+    }
+
+    /**
+     * Compute an adjusted CVSSv3 baseScore that ensures that CRITICAL CVSSv3 scores will win over HIGH CVSSv2 and CRITICAL
+     * unscored severities to allow for a best-effort sorting that enables the report to list a reliable 'highest severity'
+     * in the report.
+     *
+     * @param cvssV3BaseScore The cvssV3 baseScore severity of a vulnerability
+     * @return The cvssV3 baseScore, adjusted if necessary in order to guarantee that CVSSv3 CRITICAL scores will rate higher than
+     * CVSSv2 HIGH, unscored critical severities and unscored unrecognized severities (which are assumed for sorting to be of a
+     * critical nature)
+     * @see #estimatedSortAdjustedCVSSv3(String)
+     */
+    public static float sortAdjustedCVSSv3BaseScore(final float cvssV3BaseScore) {
+        if (cvssV3BaseScore >= 9.0f) {
+            return cvssV3BaseScore+1.3f;
+        }
+        return cvssV3BaseScore;
+    }
+
+    private enum Severity {
+        CRITICAL, ASSUMED_CRITICAL, HIGH, MEDIUM, LOW, INFO;
+
+        public static Severity forUnscored(String value) {
+            switch (value == null ? "none" : value.toLowerCase()) {
+            case "critical":
+                return CRITICAL;
+            case "high":
+                return HIGH;
+            case "moderate":
+            case "medium":
+                return MEDIUM;
+            case "info":
+            case "informational":
+                return INFO;
+            case "low":
+            case "unknown":
+            case "none":
+                return LOW;
+            default:
+                return ASSUMED_CRITICAL;
+            }
         }
     }
 }
