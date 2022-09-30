a446d187527343ee34b94a9084c045cbefd3afba
Jeremy Long <jeremy.long@gmail.com>
2022-09-30 06:15:46 -0400

    fix typo

--- core/src/main/java/org/owasp/dependencycheck/data/update/cisa/package-info.java
+++ core/src/main/java/org/owasp/dependencycheck/data/update/cisa/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Contains classes used to parse the CISA Known Exploitable Vulnerability Catalog.<br><br>
+ * Contains classes used to parse the CISA Known Exploited Vulnerability Catalog.<br><br>
  *
  */
 package org.owasp.dependencycheck.data.update.cisa;
