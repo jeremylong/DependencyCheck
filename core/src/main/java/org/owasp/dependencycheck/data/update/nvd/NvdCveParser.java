ebf4ecee99ce8de4243b2e1ee4f3643da1ba23b5
Jeremy Long <jeremy.long@gmail.com>
2023-01-09 07:14:19 -0500

    fix: correct issues with merge conflicts Apparently fixing a merge conflict in the github UI was a bad idea...

--- core/src/main/java/org/owasp/dependencycheck/data/update/nvd/NvdCveParser.java
+++ core/src/main/java/org/owasp/dependencycheck/data/update/nvd/NvdCveParser.java
@@ -46,6 +46,7 @@ import org.owasp.dependencycheck.data.nvd.json.DefCveItem;
 import org.owasp.dependencycheck.data.nvd.ecosystem.CveEcosystemMapper;
 import org.owasp.dependencycheck.data.update.exception.UpdateException;
 import org.owasp.dependencycheck.utils.Settings;
+import org.owasp.dependencycheck.utils.Utils;
 
 /**
  * Parser and processor of NVD CVE JSON data feeds.
@@ -90,7 +91,7 @@ public final class NvdCveParser {
         LOGGER.debug("Parsing " + file.getName());
 
         final Module module;
-        if (getJavaVersion() <= 8) {
+        if (Utils.getJavaVersion() <= 8) {
             module = new AfterburnerModule();
         } else {
             module = new BlackbirdModule();
@@ -123,24 +124,6 @@ public final class NvdCveParser {
             LOGGER.debug("Error extracting the NVD JSON data from: " + file, ex);
             throw new UpdateException("Unable to find the NVD CVE file to parse", ex);
         }
-    }
-
-    /**
-     * Returns the Java major version as a whole number.
-     *
-     * @return the Java major version as a whole number
-     */
-    private static int getJavaVersion() {
-        String version = System.getProperty("java.specification.version");
-        if (version.startsWith("1.")) {
-            version = version.substring(2, 3);
-        } else {
-            final int dot = version.indexOf(".");
-            if (dot != -1) {
-                version = version.substring(0, dot);
-            }
-        }
-        return Integer.parseInt(version);
     }
 
     void init(JsonParser parser) throws IOException {
