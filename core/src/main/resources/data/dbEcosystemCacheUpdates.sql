743ccf535a3a6faedd079593284f97f3c976203c
Jeremy Long <jeremy.long@gmail.com>
2023-03-23 06:14:46 -0400

    fix: SQL Syntax for Oracle (#5590)

--- core/src/main/resources/data/dbEcosystemCacheUpdates.sql
+++ core/src/main/resources/data/dbEcosystemCacheUpdates.sql
@@ -1,6 +1,6 @@
-UPDATE cpeEcosystemCache set ecosystem='MULTIPLE' where vendor = 'apache' and product = 'hadoop' and ecosystem != 'MULTIPLE';
-UPDATE cpeEcosystemCache set ecosystem='MULTIPLE' where vendor = 'apache' and product = 'ranger' and ecosystem != 'MULTIPLE';
-UPDATE cpeEcosystemCache set ecosystem='NATIVE' where vendor = 'git' and product = 'git' and ecosystem != 'NATIVE';
-UPDATE cpeEcosystemCache set ecosystem='NATIVE' where vendor = 'python' and product = 'python' and ecosystem != 'NATIVE';
-UPDATE cpeEcosystemCache set ecosystem='NATIVE' where vendor = 'python_software_foundation' and product = 'python' and ecosystem != 'NATIVE';
-UPDATE cpeEcosystemCache set ecosystem='NATIVE' where vendor = 'python' and product = 'python' and ecosystem != 'NATIVE';
+UPDATE cpeEcosystemCache set ecosystem='MULTIPLE' where vendor = 'apache' and product = 'hadoop' and ecosystem != 'MULTIPLE'
+UPDATE cpeEcosystemCache set ecosystem='MULTIPLE' where vendor = 'apache' and product = 'ranger' and ecosystem != 'MULTIPLE'
+UPDATE cpeEcosystemCache set ecosystem='NATIVE' where vendor = 'git' and product = 'git' and ecosystem != 'NATIVE'
+UPDATE cpeEcosystemCache set ecosystem='NATIVE' where vendor = 'python' and product = 'python' and ecosystem != 'NATIVE'
+UPDATE cpeEcosystemCache set ecosystem='NATIVE' where vendor = 'python_software_foundation' and product = 'python' and ecosystem != 'NATIVE'
+UPDATE cpeEcosystemCache set ecosystem='NATIVE' where vendor = 'python' and product = 'python' and ecosystem != 'NATIVE'
