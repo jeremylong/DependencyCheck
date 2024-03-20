42adde4b37f4b2dd5ef63501f696bc452f4adb65
Jeremy Long <jeremy.long@gmail.com>
2024-03-20 06:23:56 -0400

    fix: typo (#6526)

--- core/src/main/java/org/owasp/dependencycheck/data/update/NvdApiDataSource.java
+++ core/src/main/java/org/owasp/dependencycheck/data/update/NvdApiDataSource.java
@@ -378,7 +378,7 @@ public class NvdApiDataSource implements CachedWebDataSource {
                     if (key != null) {
                         msg = "Error updating the NVD Data; the NVD returned a 403 or 404 error\n\nPlease ensure your API Key is valid; "
                                 + "see https://github.com/jeremylong/Open-Vulnerability-Project/tree/main/vulnz#api-key-is-used-and-a-403-or-404-error-occurs\n\n"
-                                + "If you NVD API Key is valid try increasing the NVD API Delay.\n\n"
+                                + "If your NVD API Key is valid try increasing the NVD API Delay.\n\n"
                                 + "If this is ocurring in a CI environment";
                     } else {
                         msg = "Error updating the NVD Data; the NVD returned a 403 or 404 error\n\nConsider using an NVD API Key; "
